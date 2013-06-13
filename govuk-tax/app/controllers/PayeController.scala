ea8d67507e70cb9a3156d0c3f15dd951850ba883
Duncan Crawford <duncan@tandemconsulting.eu>
2013-06-13 14:32:54 +0100

    remove unnecessary val

--- govuk-tax/app/controllers/PayeController.scala
+++ govuk-tax/app/controllers/PayeController.scala
@@ -1,8 +1,8 @@
 package controllers
 
-import microservice.paye.domain._
-
 class PayeController extends BaseController with ActionWrappers {
+
+  import microservice.paye.domain.{Employment, Benefit, PayeRoot, PayeRegime}
 
   def home = AuthorisedForAction[PayeRegime] {
     implicit user =>
@@ -10,13 +10,12 @@ class PayeController extends BaseController with ActionWrappers {
 
         // this is safe, the AuthorisedForAction wrapper will have thrown Unauthorised if the PayeRoot data isn't present
         val payeData = user.regime.paye.get
-        val hasBenefits = !payeData.benefits.isEmpty
 
         Ok(views.html.home(
           name = payeData.name,
           employments = payeData.employments,
           taxCodes = payeData.taxCodes,
-          hasBenefits = hasBenefits)
+          hasBenefits = !payeData.benefits.isEmpty)
         )
   }
 
@@ -24,16 +23,10 @@ class PayeController extends BaseController with ActionWrappers {
     implicit user =>
       implicit request =>
         val root: PayeRoot = user.regime.paye.get
-
-        val benefits = root.benefits
-        val employments = root.employments
-
-        Ok(views.html.benefits(matchBenefitWithCorrespondingEmployment(benefits, employments)))
+        Ok(views.html.benefits(matchBenefitWithCorrespondingEmployment(root.benefits, root.employments)))
   }
 
   private def matchBenefitWithCorrespondingEmployment(benefits: Seq[Benefit], employments: Seq[Employment]): Seq[(Benefit, Employment)] = {
-
-    // TODO Refactor this with a simpler solution. EG: use collect instead of flatMap
     benefits.flatMap(benefit => {
       val correspondingEmployment = employments.find(_.sequenceNumber.equals(benefit.employmentSequenceNumber))
       correspondingEmployment match {
