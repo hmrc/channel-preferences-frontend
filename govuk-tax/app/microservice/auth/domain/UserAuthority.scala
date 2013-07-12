dad40ffc3c877450624a9ec8daab687c7aeffaca
Jakob Grunig <jakob.grunig@digital.cabinet-office.gov.uk>
2013-07-12 16:03:08 +0100

    Fixed json inconsistencies in ggw

--- govuk-tax/app/microservice/auth/domain/UserAuthority.scala
+++ govuk-tax/app/microservice/auth/domain/UserAuthority.scala
@@ -2,4 +2,5 @@ package microservice.auth.domain
 
 import org.joda.time.DateTime
 
-case class UserAuthority(id: String, regimes: Map[String, String], previouslyLoggedInAt: Option[DateTime])
+case class UserAuthority(id: String, regimes: Regimes, previouslyLoggedInAt: Option[DateTime])
+case class Regimes(paye: Option[String] = None, sa: Option[String] = None, vat: Set[String] = Set(), ct: Set[String] = Set())
--- govuk-tax/app/microservice/ggw/GgwMicroService.scala
+++ govuk-tax/app/microservice/ggw/GgwMicroService.scala
@@ -12,7 +12,7 @@ class GgwMicroService extends MicroService {
   override val serviceUrl = MicroServiceConfig.ggwServiceUrl
 
   implicit object CredentialsWrites extends Writes[Credentials] {
-    def writes(c: Credentials): JsValue = JsObject(Seq("username" -> JsString(c.userId), "password" -> JsString(c.password)))
+    def writes(c: Credentials): JsValue = JsObject(Seq("userId" -> JsString(c.userId), "password" -> JsString(c.password)))
   }
 
   def login(credentials: Credentials) = {
