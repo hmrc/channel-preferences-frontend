bb9f4cf41e7f053e4308c9e0cfd72090639b2711
Rory Powis <rory@hikkendry.com>
2013-07-29 14:10:22 +0100

    Add Selenium test hooks to HTML for home and benefits pages

--- govuk-tax/app/controllers/PayeController.scala
+++ govuk-tax/app/controllers/PayeController.scala
@@ -31,6 +31,7 @@ class PayeController extends BaseController with ActionWrappers {
       implicit request =>
         val benefits = user.regimes.paye.get.benefits
         val employments = user.regimes.paye.get.employments
+	// TODO: add lowercase hyphenated formatter
         Ok(paye_benefit_home(matchBenefitWithCorrespondingEmployment(benefits, employments)))
   }
 
