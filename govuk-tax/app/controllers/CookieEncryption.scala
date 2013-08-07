a74a282816746694713e774c7f9844e095ecf13c
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2013-08-07 18:50:31 +0100

    Using crypto functions from secure utils lib

--- govuk-tax/app/controllers/CookieEncryption.scala
+++ govuk-tax/app/controllers/CookieEncryption.scala
@@ -2,29 +2,23 @@ package controllers
 
 import play.api.Play
 import org.apache.commons.codec.binary.Base64
-import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
-import java.nio.charset.StandardCharsets
+import uk.gov.hmrc.secure.{ SymmetricDecrypter, SymmetricEncrypter }
 
 trait CookieEncryption {
 
-  private lazy val cookieEncryptionKey = Base64.decodeBase64(Play.current.configuration.getString("cookie.encryption.key").get.getBytes("UTF-8"))
+  private lazy val cookieEncryptionKey = Base64.decodeBase64(
+    Play.current.configuration.getString("cookie.encryption.key").get.getBytes("UTF-8"))
 
   private lazy val secretKey = new SecretKeySpec(cookieEncryptionKey, "AES")
 
-  def encrypt(id: String) = {
-    val cipher = Cipher.getInstance("AES")
+  private val encrypter = new SymmetricEncrypter
 
-    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-    new String(Base64.encodeBase64(cipher.doFinal(id.getBytes(StandardCharsets.UTF_8))), StandardCharsets.UTF_8)
-  }
+  private val decrypter = new SymmetricDecrypter
 
-  def decrypt(id: String): String = {
-    val cipher = Cipher.getInstance("AES")
+  def encrypt(id: String) = encrypter.encrypt(id, secretKey)
 
-    cipher.init(Cipher.DECRYPT_MODE, secretKey)
-    new String(cipher.doFinal(Base64.decodeBase64(id.getBytes(StandardCharsets.UTF_8))))
-  }
+  def decrypt(id: String): String = decrypter.decrypt(id, secretKey)
 
   def decrypt(id: Option[String]): Option[String] = id.map(decrypt)
 }
