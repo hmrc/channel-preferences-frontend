416174b0d446db871ee37362b2858e8f99b5c98b
Duncan Crawford <duncan@tandemconsulting.eu>
2013-08-07 08:39:15 +0100

    tidy up the build files to conform to the other projects

---
+++ govuk-tax/project/GovUkTaxBuild.scala
@@ -0,0 +1,55 @@
+import sbt._
+import Keys._
+import play.Project._
+import net.litola.SassPlugin
+import com.typesafe.sbt.SbtScalariform._
+
+object GovUkTaxBuild extends Build {
+
+  val appName = "govuk-tax"
+
+  val appDependencies = Seq(
+    anorm,
+    Dependencies.Compile.nscalaTime,
+    Dependencies.Compile.json4sExt,
+    Dependencies.Compile.json4sJackson,
+    Dependencies.Compile.guava,
+    Dependencies.Compile.playMetrics,
+    Dependencies.Compile.metricsGraphite,
+
+    Dependencies.Test.junit,
+    Dependencies.Test.scalaTest,
+    Dependencies.Test.mockito
+  )
+
+  lazy val govukTax = play.Project(
+    appName,
+    Version.thisApp, appDependencies,
+    settings = Common.commonSettings ++ SassPlugin.sassSettings
+  ).settings(publishArtifact := false)
+
+}
+
+object Common {
+  val commonSettings = Defaults.defaultSettings ++
+    scalariformSettings ++
+    Seq(
+      organization := "uk.gov.hmrc",
+      version := Version.thisApp,
+      scalaVersion := Version.scala,
+      scalacOptions ++= Seq(
+        "-unchecked",
+        "-deprecation",
+        "-Xlint",
+        "-language:_",
+        "-target:jvm-1.7",
+        "-encoding", "UTF-8"
+      ),
+      resolvers ++= Repositories.resolvers,
+      retrieveManaged := true,
+      testOptions in Test <+= (target in Test) map {
+        t => Tests.Argument(TestFrameworks.ScalaTest, "junitxml(directory=\"%s\")" format (t / "test-reports"))
+      }
+    ) ++
+    Repositories.publishingSettings
+}
