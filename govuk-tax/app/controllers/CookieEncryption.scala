f0f37eab07c66563f0ed772b945231570dc83d1d
petrnejedly <pnejedly@equalexperts.com>
2013-08-09 10:50:30 +0100

    refactored out encryption for re-use

--- govuk-tax/app/controllers/CookieEncryption.scala
+++ govuk-tax/app/controllers/CookieEncryption.scala
@@ -3,22 +3,11 @@ package controllers
 import play.api.Play
 import org.apache.commons.codec.binary.Base64
 import javax.crypto.spec.SecretKeySpec
+import service.Encryption
 import uk.gov.hmrc.secure.{ SymmetricDecrypter, SymmetricEncrypter }
 
-trait CookieEncryption {
+trait CookieEncryption extends Encryption {
 
-  private lazy val cookieEncryptionKey = Base64.decodeBase64(
-    Play.current.configuration.getString("cookie.encryption.key").get.getBytes("UTF-8"))
+  override lazy val encryptionKey = Play.current.configuration.getString("cookie.encryption.key").get
 
-  private lazy val secretKey = new SecretKeySpec(cookieEncryptionKey, "AES")
-
-  private val encrypter = new SymmetricEncrypter
-
-  private val decrypter = new SymmetricDecrypter
-
-  def encrypt(id: String) = encrypter.encrypt(id, secretKey)
-
-  def decrypt(id: String): String = decrypter.decrypt(id, secretKey)
-
-  def decrypt(id: Option[String]): Option[String] = id.map(decrypt)
 }
