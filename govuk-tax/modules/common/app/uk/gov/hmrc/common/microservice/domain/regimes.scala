aa432b6bf355d9f86d3fee85fbf95b7882910d85
Eric Vlaanderen <eric.vlaanderen@digital.cabinet-office.gov.uk>
2013-08-12 14:59:43 +0100

    Move transactions to PayeRoot

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/domain/regimes.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/domain/regimes.scala
@@ -15,6 +15,6 @@ abstract class RegimeRoot
 
 case class User(user: String, userAuthority: UserAuthority, regimes: RegimeRoots, nameFromGovernmentGateway: Option[String] = None, decryptedToken: Option[String])
 
-case class RegimeRoots(paye: Option[PayeRoot], sa: Option[SaRoot], vat: Option[String], transaction: Option[String]) {
+case class RegimeRoots(paye: Option[PayeRoot], sa: Option[SaRoot], vat: Option[String]) {
   def hasBusinessTaxRegime: Boolean = sa.isDefined || vat.isDefined
 }
