781d856c8b3145f0052d25c2a92a8a58953aea59
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2013-09-05 14:54:42 +0100

    Introduced conf file for preview environment containing env specicif host names, secret keyds, etc

--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "127.0.0.1 $(hostname)" > /etc/hosts
-exec sh -ex /src/govuk-tax-0.0.1-SNAPSHOT/start -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/govuk-tax-logger.xml
+exec sh -ex /src/govuk-tax-0.0.1-SNAPSHOT/start -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/govuk-tax-logger.xml -Dconfig.resource=preview
