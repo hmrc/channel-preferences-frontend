de4ffe4189559883d4d2b1ac61e038ceb19e787b
Dave Sammut <dave.sammut@digital.cabinet-office.gov.uk>
2013-09-10 16:39:16 +0100

    HMTB-33 populated model for SA and VAT using AccountSummary model

--- govuk-tax/modules/business-tax/app/controllers/bt/BusinessTaxController.scala
+++ govuk-tax/modules/business-tax/app/controllers/bt/BusinessTaxController.scala
@@ -5,6 +5,7 @@ import uk.gov.hmrc.microservice.auth.domain._
 import uk.gov.hmrc.microservice.domain._
 import controllers.common._
 import uk.gov.hmrc.common.PortalDestinationUrlBuilder
+import config.PortalConfig
 
 class BusinessTaxController extends BaseController with ActionWrappers with CookieEncryption with SessionTimeoutWrapper {
 
@@ -17,12 +18,32 @@ class BusinessTaxController extends BaseController with ActionWrappers with Cook
         val encodedGovernmentGatewayToken = user.decryptedToken.get
         val businessUser = BusinessUser(user.regimes, userAuthority.utr, userAuthority.vrn, userAuthority.ctUtr, userAuthority.empRef, user.nameFromGovernmentGateway.getOrElse(""), userAuthority.previouslyLoggedInAt, encodedGovernmentGatewayToken)
 
-        val portalHref = PortalDestinationUrlBuilder.build(request, user)("home")
-        Ok(views.html.business_tax_home(businessUser, portalHref))
+        val buildPortalUrl = PortalDestinationUrlBuilder.build(request, user) _
+        val portalHref = buildPortalUrl("home")
+
+        val saRegime = buildSaAccountSummary(buildPortalUrl, "some data from the SA -> CESA Hod")
+        val vatRegime = buildVatAccountSummary(buildPortalUrl, "some data from the VAT -> VMF Hod")
+
+        val accountSummaries = AccountSummaries(Seq(saRegime, vatRegime))
+
+        Ok(views.html.business_tax_home(businessUser, portalHref, accountSummaries))
 
   })
+
+  def buildSaAccountSummary(buildPortalUrl: String => String, data: String): AccountSummary = {
+    val links = Seq(Link(buildPortalUrl("saViewAccountDetails"), "PORTAL: Sa View Account Details", Some(PortalConfig.ssoUrl)))
+    AccountSummary("SA", Seq(data), links)
+  }
+
+  def buildVatAccountSummary(buildPortalUrl: String => String, data: String): AccountSummary = {
+    val links = Seq(Link(buildPortalUrl("home"), "Take me to the Portal Home", Some(PortalConfig.ssoUrl)))
+    AccountSummary("VAT", Seq(data), links)
+  }
 
 }
 
 case class BusinessUser(regimeRoots: RegimeRoots, utr: Option[Utr], vrn: Option[Vrn], ctUtr: Option[Utr], empRef: Option[EmpRef], name: String, previouslyLoggedInAt: Option[DateTime], encodedGovernmentGatewayToken: String)
 
+case class AccountSummaries(regimes: Seq[AccountSummary])
+case class AccountSummary(regimeName: String, messages: Seq[String], links: Seq[Link])
+case class Link(href: String, text: String, ssoUrl: Option[String])
