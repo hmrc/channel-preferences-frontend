9e947dac6df0842b6f45f159064832d8da812087
Charlotte Philippe <charlotte.philippe@digital.cabinet-office.gov.uk>
2013-09-11 17:01:07 +0100

    Removing scalaform plugin

--- govuk-tax/project/GovUkTaxBuild.scala
+++ govuk-tax/project/GovUkTaxBuild.scala
@@ -2,7 +2,6 @@ import sbt._
 import Keys._
 import play.Project._
 import net.litola.SassPlugin
-import com.typesafe.sbt.SbtScalariform._
 
 object GovUkTaxBuild extends Build {
 
@@ -78,7 +77,6 @@ object GovUkTaxBuild extends Build {
 
 object Common {
   val commonSettings = Defaults.defaultSettings ++
-    scalariformSettings ++
     Seq(
       organization := "uk.gov.hmrc",
       version := Version.thisApp,
