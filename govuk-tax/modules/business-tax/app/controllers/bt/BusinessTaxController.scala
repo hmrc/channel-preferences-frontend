4422f1a9bf3efb5a360dd1e0e5e612ceba1874c0
Howard Branch <howard.branch@digital.cabinet-office.gov.uk>
2013-09-12 10:36:49 +0100

    Moved AccountSummary build code into separate builders

--- govuk-tax/modules/business-tax/app/controllers/bt/BusinessTaxController.scala
+++ govuk-tax/modules/business-tax/app/controllers/bt/BusinessTaxController.scala
@@ -7,9 +7,9 @@ import controllers.common._
 import uk.gov.hmrc.common.PortalDestinationUrlBuilder
 import uk.gov.hmrc.common.microservice.vat.domain.VatDomain.{ VatAccountSummary, VatRoot }
 import views.helpers.{ StringMessage, LinkMessage, StringOrLinkMessage }
+import controllers.bt.regimeViews.{SaAccountSummaryViewBuilder, VatAccountSummaryViewBuilder}
 
 class BusinessTaxController extends BaseController with ActionWrappers with SessionTimeoutWrapper {
-  implicit def translate(value: String): StringMessage = StringMessage(value)
 
   def home = WithSessionTimeoutValidation(AuthorisedForGovernmentGatewayAction() {
     implicit user =>
@@ -22,44 +22,13 @@ class BusinessTaxController extends BaseController with ActionWrappers with Sess
         val buildPortalUrl = PortalDestinationUrlBuilder.build(request, user) _
         val portalHref = buildPortalUrl("home")
 
-        val saRegime = buildSaAccountSummary(buildPortalUrl, "some data from the SA -> CESA Hod")
-        val vatRegime = buildVatAccountSummary(buildPortalUrl, user.regimes.vat, userAuthority.vrn)
-
+        val saRegime = SaAccountSummaryViewBuilder(buildPortalUrl, "some data from the SA -> CESA Hod", saMicroService).build
+        val vatRegime = VatAccountSummaryViewBuilder(buildPortalUrl, user.regimes.vat, userAuthority.vrn, vatMicroService).build
         val accountSummaries = AccountSummaries(Seq(saRegime, vatRegime).flatten)
 
         Ok(views.html.business_tax_home(businessUser, portalHref, accountSummaries))
 
   })
-
-  def buildSaAccountSummary(buildPortalUrl: String => String, data: String): Option[AccountSummary] = {
-    val links = Seq(LinkMessage(buildPortalUrl("saViewAccountDetails"), "PORTAL: Sa View Account Details"))
-    Some(AccountSummary("SA", Seq.empty, links))
-  }
-
-  def buildVatAccountSummary(buildPortalUrl: String => String, vatRootOption: Option[VatRoot], vrn: Option[Vrn]): Option[AccountSummary] = {
-    vatRootOption.map {
-      vatRoot: VatRoot =>
-        val accountSummary: Option[VatAccountSummary] = vatRootOption.get.accountSummary(vatMicroService)
-        val accountValueOption: Option[BigDecimal] = for {
-          accountSummaryValue <- accountSummary
-          accountBalance <- accountSummaryValue.accountBalance
-          amount <- accountBalance.amount
-        } yield amount
-
-        val links = Seq(LinkMessage(buildPortalUrl("vatAccountDetails"), "View your account on HMRC Portal"))
-
-        accountValueOption match {
-          case Some(accountValue) => {
-            AccountSummary("VAT", Seq("vat.message.0" -> List(vrn.get.vrn),
-              "vat.message.1" -> List(accountValue.toString())), links)
-          }
-          case None => {
-            AccountSummary("VAT", Seq("vat.error.message.1" -> List.empty), Seq.empty)
-          }
-        }
-    }
-  }
-
 }
 
 case class BusinessUser(regimeRoots: RegimeRoots, utr: Option[Utr], vrn: Option[Vrn], ctUtr: Option[Utr], empRef: Option[EmpRef], name: String, previouslyLoggedInAt: Option[DateTime], encodedGovernmentGatewayToken: String)
---
+++ govuk-tax/modules/business-tax/app/controllers/bt/regimeViews/VatAccountSummaryViewBuilder.scala
@@ -0,0 +1,36 @@
+package controllers.bt.regimeViews
+
+import uk.gov.hmrc.common.microservice.vat.domain.VatDomain.{VatAccountSummary, VatRoot}
+import uk.gov.hmrc.microservice.auth.domain.Vrn
+import views.helpers.{StringMessage, LinkMessage}
+import controllers.bt.AccountSummary
+import uk.gov.hmrc.common.microservice.vat.VatMicroService
+
+case class VatAccountSummaryViewBuilder(buildPortalUrl: String => String, vatRootOption: Option[VatRoot], vrn: Option[Vrn], vatMicroService: VatMicroService) {
+  implicit def translate(value: String): StringMessage = StringMessage(value)
+
+  def build: Option[AccountSummary] = {
+    vatRootOption.map {
+      vatRoot: VatRoot =>
+        val accountSummary: Option[VatAccountSummary] = vatRootOption.get.accountSummary(vatMicroService)
+        val accountValueOption: Option[BigDecimal] = for {
+          accountSummaryValue <- accountSummary
+          accountBalance <- accountSummaryValue.accountBalance
+          amount <- accountBalance.amount
+        } yield amount
+
+        val links = Seq(LinkMessage(buildPortalUrl("vatAccountDetails"), "View your account on HMRC Portal"))
+
+        accountValueOption match {
+          case Some(accountValue) => {
+            AccountSummary("VAT", Seq("vat.message.0" -> List(vrn.get.vrn),
+              "vat.message.1" -> List(accountValue.toString())), links)
+          }
+          case None => {
+            AccountSummary("VAT", Seq("vat.error.message.1" -> List.empty), Seq.empty)
+          }
+        }
+    }
+  }
+}
+
