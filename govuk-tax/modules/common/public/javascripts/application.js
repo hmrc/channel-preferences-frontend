5ec867e70db2fd4162597822d1b18fc7d4a1b142
Rory Powis <rory@hikkendry.com>
2013-09-12 16:31:56 +0100

    Refactor SSO javascript to make it global and dynamic

--- govuk-tax/modules/common/public/javascripts/application.js
+++ govuk-tax/modules/common/public/javascripts/application.js
@@ -1,8 +1,54 @@
-$(document).on('click', '[data-regime]', function() {
-  var regime = $(this).data('regime');
-  _gaq.push(['_trackPageview', '/' + regime]);
+/**
+ * Attach a one-time event handler for all global links
+ */
+$(document).on('click', 'a', function(e) {
+  var $target = $(this),
+      linkHost = this.host,
+      a = document.createElement('a');
+
+  a.href = ssoUrl;
+  ssoHost = a.host;
+
+  if(linkHost === ssoHost) {
+    var successful = true;
+
+    $.ajax({
+	url: '/ssoout',
+	data: { destinationUrl : $target[0].href },
+	type: 'GET',
+	success: function(data) {
+
+	    var form = document.createElement('form');
+	    form.method = 'POST';
+	    form.action = ssoUrl;
+
+	    var payload = document.createElement('input');
+	    payload.type = 'hidden';
+	    payload.name = 'payload';
+	    payload.value = data;
+
+	    document.body.appendChild(form);
+	    form.appendChild(payload);
+
+	    // POST form
+	    form.submit();
+	},
+	error: function() { successful = false; }
+    });
+
+    // cancel link click event if everything is successful
+    return !successful;
+  }
+
+  if( $target.data('regime') ) {
+    var regime = $(this).data('regime');
+    _gaq.push(['_trackPageview', '/' + regime]);
+  }
 });
 
+/**
+ * DOM ready
+ */
 $(document).ready(function() {
   $('.print-link a').attr('target', '_blank');
 
