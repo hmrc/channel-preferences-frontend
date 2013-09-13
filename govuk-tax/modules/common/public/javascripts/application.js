524bb8e8cd9d81c3499519141a77e8f80d020c97
Rory Powis <rory@hikkendry.com>
2013-09-13 17:15:55 +0100

    Add cache: false to SSO javascript

--- govuk-tax/modules/common/public/javascripts/application.js
+++ govuk-tax/modules/common/public/javascripts/application.js
@@ -13,28 +13,28 @@ $(document).on('click', 'a', function(e) {
     var successful = true;
 
     $.ajax({
-	url: '/ssoout',
-	data: { destinationUrl : $target[0].href },
-	type: 'GET',
-    async: false,
-	success: function(data) {
+      url: '/ssoout',
+      data: { destinationUrl : $target[0].href },
+      type: 'GET',
+      async: false,
+      cache: false,
+      success: function(data, status, jqXHR) {
+	var form = document.createElement('form');
+	form.method = 'POST';
+	form.action = ssoUrl;
 
-	    var form = document.createElement('form');
-	    form.method = 'POST';
-	    form.action = ssoUrl;
+	var payload = document.createElement('input');
+	payload.type = 'hidden';
+	payload.name = 'payload';
+	payload.value = data;
 
-	    var payload = document.createElement('input');
-	    payload.type = 'hidden';
-	    payload.name = 'payload';
-	    payload.value = data;
+	document.body.appendChild(form);
+	form.appendChild(payload);
 
-	    document.body.appendChild(form);
-	    form.appendChild(payload);
-
-	    // POST form
-	    form.submit();
-	},
-	error: function() { successful = false; }
+	// POST form
+	form.submit();
+      },
+      error: function() { successful = false; }
     });
 
     // cancel link click event if everything is successful
