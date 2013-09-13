b5b35b00d74b56ba06963bf0db857ac595f8859e
Howard Branch <howard.branch@digital.cabinet-office.gov.uk>
2013-09-13 09:57:25 +0100

    updated links, minor tidy

--- govuk-tax/modules/business-tax/app/controllers/bt/regimeViews/SaAccountSummaryViewBuilder.scala
+++ govuk-tax/modules/business-tax/app/controllers/bt/regimeViews/SaAccountSummaryViewBuilder.scala
@@ -1,14 +1,13 @@
 package controllers.bt.regimeViews
 
-import views.helpers.{RenderableLinkMessage, RenderableStringMessage }
-import views.helpers.LinkMessage
+import views.helpers.{RenderableMessage, LinkMessage}
 import controllers.bt.AccountSummary
 import uk.gov.hmrc.microservice.sa.SaMicroService
 
 case class SaAccountSummaryViewBuilder(buildPortalUrl: String => String, data: String, saMicroService: SaMicroService) {
 
   def build(): Option[AccountSummary] = {
-    val links = Seq[RenderableLinkMessage](LinkMessage(buildPortalUrl("saViewAccountDetails"), "PORTAL: Sa View Account Details"))
+    val links = Seq[RenderableMessage](LinkMessage(buildPortalUrl("saViewAccountDetails"), "PORTAL: Sa View Account Details"))
     Some(AccountSummary("SA", Seq.empty, links))
   }
 }
