1634eb3e9da145eed7108d079cafe7923ebc3ebc
Rory Powis <rory@hikkendry.com>
2013-09-27 18:11:33 +0100

    Allow fieldGroup to accept dateFields
    Conflicts:
    	govuk-tax/modules/paye/app/views/paye/add_car_benefit_form.scala.html

--- govuk-tax/modules/common/app/views/helpers/model.scala
+++ govuk-tax/modules/common/app/views/helpers/model.scala
@@ -1,7 +1,18 @@
 package views.helpers
 
-case class InputType(inputType: String, key: String, value: String, divClass: Option[String] = None, labelClass: Option[String] = None, inputClass: Option[String] = None)
+trait FieldType {
+  val inputType: String
+  val key: String
+  val value: String
+  val divClass: Option[String]
+  val labelClass: Option[String]
+  val inputClass: Option[String]
+}
 
-case class RadioButton(key: String, value: String, divClass: Option[String] = None, labelClass: Option[String] = None)
+case class InputType(inputType: String, key: String, value: String,  divClass: Option[String] = None, labelClass: Option[String] = None, inputClass: Option[String] = None) extends FieldType
 
-case class FormField(field: play.api.data.Field, inputs: Seq[InputType])
+case class RadioButton(key: String, value: String, divClass: Option[String] = None, labelClass: Option[String] = None, inputClass: Option[String] = None, inputType: String = "radio") extends FieldType
+
+case class DateControl(inputType: String = "", key: String ="", value: String = "", divClass: Option[String] = None, labelClass: Option[String] = None, inputClass: Option[String] = None) extends FieldType
+
+case class FormField(field: play.api.data.Field, inputs: Seq[FieldType])
