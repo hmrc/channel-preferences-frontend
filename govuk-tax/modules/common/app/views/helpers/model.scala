4fe70e867be2a83b0c4e18d11c82c53fbc867c3d
Rory Powis <rory@hikkendry.com>
2013-09-27 16:41:02 +0100

    Add fieldGroup helper for wrapping multiple inputs
    Conflicts:
    	govuk-tax/modules/paye/app/views/paye/add_car_benefit_form.scala.html

--- govuk-tax/modules/common/app/views/helpers/model.scala
+++ govuk-tax/modules/common/app/views/helpers/model.scala
@@ -1,3 +1,7 @@
 package views.helpers
 
+case class InputType(inputType: String, key: String, value: String, divClass: Option[String] = None, labelClass: Option[String] = None, inputClass: Option[String] = None)
+
 case class RadioButton(key: String, value: String, divClass: Option[String] = None, labelClass: Option[String] = None)
+
+case class FormField(field: play.api.data.Field, inputs: Seq[InputType])
