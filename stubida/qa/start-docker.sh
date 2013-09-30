6bf540735afb86cff1dc32b2da6e8d7931279181
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2013-09-30 15:28:10 +0100

    Fixed conf directory structure for stubida

--- stubida/qa/start-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#echo "127.0.0.1 $(hostname)" > /etc/hosts
-exec sh -ex /src/govuk-tax-0.0.1-SNAPSHOT/start -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/govuk-tax-logger.xml -Dconfig.resource=/qa-stubida.conf -Dgovuk-tax.Prod.services.saml.host=hod.service
