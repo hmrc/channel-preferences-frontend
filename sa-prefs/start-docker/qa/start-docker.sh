c733593a51c6a9afd62ee7b1822e81241d354ca0
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2013-09-30 11:49:45 +0100

    Fixed conf file names for prefs

--- sa-prefs/start-docker/qa/start-docker.sh
+++ sa-prefs/start-docker/qa/start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #echo "127.0.0.1 $(hostname)" > /etc/hosts
-exec sh -ex /src/sa-prefs-0.0.1-SNAPSHOT/start -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/sa-prefs-logger.xml -Dconfig.resource=/CONF_FILE
+exec sh -ex /src/sa-prefs-0.0.1-SNAPSHOT/start -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/sa-prefs-logger.xml -Dconfig.resource=/qa.conf
