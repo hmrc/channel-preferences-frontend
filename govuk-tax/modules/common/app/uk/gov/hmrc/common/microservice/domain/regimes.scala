7fd9ed172691cfb19b7deb02807074c4b7a17e17
Howard Branch <howard.branch@digital.cabinet-office.gov.uk>
2013-10-09 08:48:30 +0100

    refactor to RegimeRoots building

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/domain/regimes.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/domain/regimes.scala
@@ -19,7 +19,7 @@ abstract class TaxRegime {
 }
 
 abstract class RegimeRoot[I] {
-  def identifier  : I
+  def identifier: I
 }
 
 case class User(userId: String,
@@ -33,51 +33,49 @@ case class User(userId: String,
 }
 
 
+class RegimeRoots(payeBuilder: Option[RegimeRootBuilder[PayeRoot]],
+                  saBuilder: Option[RegimeRootBuilder[SaRoot]],
+                  vatBuilder: Option[RegimeRootBuilder[VatRoot]],
+                  epayeBuilder: Option[RegimeRootBuilder[EpayeRoot]],
+                  ctBuilder: Option[RegimeRootBuilder[CtRoot]]) {
 
-class RegimeRoots(  payeBuilder : Option[RegimeRootBuilder[PayeRoot]],
-                        saBuilder : Option[RegimeRootBuilder[SaRoot]],
-                        vatBuilder : Option[RegimeRootBuilder[VatRoot]],
-                        epayeBuilder : Option[RegimeRootBuilder[EpayeRoot]],
-                        ctBuilder : Option[RegimeRootBuilder[CtRoot]]) {
-  lazy val paye : Option[Try[PayeRoot]] = payeBuilder.map(_())
-  lazy val sa : Option[Try[SaRoot]] = saBuilder.map(_())
-  lazy val vat : Option[Try[VatRoot]] = vatBuilder.map(_())
-  lazy val epaye : Option[Try[EpayeRoot]] = epayeBuilder.map(_())
-  lazy val ct : Option[Try[CtRoot]] = ctBuilder.map(_())
+  lazy val paye: Option[Try[PayeRoot]] = payeBuilder.map(_())
+  lazy val sa: Option[Try[SaRoot]] = saBuilder.map(_())
+  lazy val vat: Option[Try[VatRoot]] = vatBuilder.map(_())
+  lazy val epaye: Option[Try[EpayeRoot]] = epayeBuilder.map(_())
+  lazy val ct: Option[Try[CtRoot]] = ctBuilder.map(_())
 
   lazy val hasBusinessTaxRegime: Boolean = sa.isDefined || vat.isDefined || epaye.isDefined || ct.isDefined
 
-  override def equals(that : Any) : Boolean = {
+  override def equals(that: Any): Boolean = {
     that match {
-      case regimeRoots : RegimeRoots => paye == regimeRoots.paye && sa == regimeRoots.sa && vat == regimeRoots.vat && epaye == regimeRoots.epaye && ct == regimeRoots.ct
+      case regimeRoots: RegimeRoots => paye == regimeRoots.paye && sa == regimeRoots.sa && vat == regimeRoots.vat && epaye == regimeRoots.epaye && ct == regimeRoots.ct
       case _ => false
     }
   }
-  override def hashCode : Int = paye.hashCode() + sa.hashCode() * 2 + vat.hashCode() * 3 + epaye.hashCode() * 5 + ct.hashCode() * 7
+
+  override def hashCode: Int = paye.hashCode() + sa.hashCode() * 2 + vat.hashCode() * 3 + epaye.hashCode() * 5 + ct.hashCode() * 7
 }
 
 object RegimeRoots {
-  def apply(paye: Option[Try[PayeRoot]] = None, sa : Option[Try[SaRoot]] = None,
-            vat : Option[Try[VatRoot]] = None, epaye : Option[Try[EpayeRoot]] = None,
-            ct : Option[Try[CtRoot]] = None) : RegimeRoots = {
-    val (_paye, _sa, _vat, _epaye, _ct) = (paye, sa, vat, epaye, ct)
-    new RegimeRoots(None, None, None, None, None) {
-      override lazy val paye = _paye
-      override lazy val sa = _sa
-      override lazy val vat = _vat
-      override lazy val epaye = _epaye
-      override lazy val ct = _ct
-    }
+  def apply(paye: Option[Try[PayeRoot]] = None, sa: Option[Try[SaRoot]] = None,
+            vat: Option[Try[VatRoot]] = None, epaye: Option[Try[EpayeRoot]] = None,
+            ct: Option[Try[CtRoot]] = None): RegimeRoots = {
+
+    val payeBuilder = paye.map(tryPayeRoot => new RegimeRootBuilder(() => tryPayeRoot.get))
+    val saBuilder = sa.map(trySaRoot => new RegimeRootBuilder(() => trySaRoot.get))
+    val vatBuilder = vat.map(tryVatRoot => new RegimeRootBuilder(() => tryVatRoot.get))
+    val epayeBuilder = epaye.map(tryEpayeRoot => new RegimeRootBuilder(() => tryEpayeRoot.get))
+    val ctBuilder = ct.map(tryCtRoot => new RegimeRootBuilder(() => tryCtRoot.get))
+    new RegimeRoots(payeBuilder, saBuilder, vatBuilder, epayeBuilder, ctBuilder)
   }
 
-  def unapply(root: RegimeRoots) : Option[(Option[Try[PayeRoot]], Option[Try[SaRoot]],
-    Option[Try[VatRoot]], Option[Try[EpayeRoot]],
-    Option[Try[CtRoot]])] = {
+  def unapply(root: RegimeRoots): Option[(Option[Try[PayeRoot]], Option[Try[SaRoot]], Option[Try[VatRoot]], Option[Try[EpayeRoot]], Option[Try[CtRoot]])] = {
     Some((root.paye, root.sa, root.vat, root.epaye, root.ct))
   }
 
-  case class RegimeRootBuilder[R] (build : () => R) {
-    def apply() : Try[R] = {
+  class RegimeRootBuilder[R](build: () => R) {
+    def apply(): Try[R] = {
       val result = Try(build())
       if (result.isFailure) Logger.error(s"Exception caught here but may be re-thrown later: ${ExceptionUtils.getFullStackTrace(result.failed.get)}")
       result
@@ -85,6 +83,9 @@ object RegimeRoots {
   }
 
 }
+
+
+
 
 
 
