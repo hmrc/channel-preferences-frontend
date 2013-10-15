316bc6c1beb81496d24ecbeb411d501a5541e746
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2013-10-15 12:34:08 +0100

    Added session timeout validation check into authenticated actions

--- govuk-tax/modules/common/test/controllers/AuthorisedForGovernmentGatewayActionSpec.scala
+++ govuk-tax/modules/common/test/controllers/AuthorisedForGovernmentGatewayActionSpec.scala
@@ -5,12 +5,11 @@ import play.api.mvc.Controller
 import uk.gov.hmrc.common.microservice.auth.AuthMicroService
 import org.mockito.Mockito.when
 import org.mockito.Mockito.reset
-import uk.gov.hmrc.common.microservice.auth.domain.{Regimes, UserAuthority}
-import play.api.test.{FakeRequest, FakeApplication, WithApplication}
+import play.api.test.{FakeRequest, WithApplication}
 import play.api.test.Helpers._
 import java.net.URI
 import org.slf4j.MDC
-import uk.gov.hmrc.common.microservice.sa.domain.SaDomain.{SaJsonRoot, SaRoot}
+import uk.gov.hmrc.common.microservice.sa.domain.SaDomain.SaRoot
 import uk.gov.hmrc.common.microservice.sa.domain.SaRegime
 import uk.gov.hmrc.common.microservice.sa.SaConnector
 import uk.gov.hmrc.common.BaseSpec
@@ -18,29 +17,49 @@ import uk.gov.hmrc.common.microservice.MockMicroServicesForTests
 import controllers.common._
 import org.scalatest.TestData
 import java.util.UUID
-import scala.util.Success
 import uk.gov.hmrc.common.microservice.epaye.EpayeConnector
 import uk.gov.hmrc.common.microservice.ct.CtConnector
 import uk.gov.hmrc.common.microservice.vat.VatConnector
-import uk.gov.hmrc.common.microservice.vat.domain.VatDomain.{VatJsonRoot, VatRoot}
-import uk.gov.hmrc.common.microservice.epaye.domain.EpayeDomain.{EpayeRoot, EpayeLinks, EpayeJsonRoot}
-import uk.gov.hmrc.common.microservice.ct.domain.CtDomain.{CtRoot, CtJsonRoot}
-import uk.gov.hmrc.domain.{SaUtr, CtUtr, EmpRef, Vrn}
+import uk.gov.hmrc.common.microservice.vat.domain.VatDomain.VatRoot
+import uk.gov.hmrc.common.microservice.epaye.domain.EpayeDomain.EpayeRoot
+import uk.gov.hmrc.common.microservice.ct.domain.CtDomain.CtRoot
+import uk.gov.hmrc.domain.EmpRef
+import controllers.common.SessionTimeoutWrapper._
+import uk.gov.hmrc.common.microservice.auth.domain.UserAuthority
+import uk.gov.hmrc.common.microservice.epaye.domain.EpayeDomain.EpayeLinks
+import uk.gov.hmrc.common.microservice.sa.domain.SaDomain.SaJsonRoot
+import uk.gov.hmrc.domain.Vrn
+import uk.gov.hmrc.common.microservice.auth.domain.Regimes
+import uk.gov.hmrc.domain.CtUtr
+import uk.gov.hmrc.domain.SaUtr
+import scala.util.Success
+import uk.gov.hmrc.common.microservice.ct.domain.CtDomain.CtJsonRoot
+import uk.gov.hmrc.common.microservice.epaye.domain.EpayeDomain.EpayeJsonRoot
+import play.api.test.FakeApplication
+import uk.gov.hmrc.common.microservice.vat.domain.VatDomain.VatJsonRoot
+import uk.gov.hmrc.utils.DateTimeUtils.now
 
-class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSugar with CookieEncryption with Controller with ActionWrappers with MockMicroServicesForTests with HeaderNames {
+class AuthorisedForGovernmentGatewayActionSpec
+  extends BaseSpec
+  with MockitoSugar
+  with CookieEncryption
+  with Controller
+  with ActionWrappers
+  with MockMicroServicesForTests
+  with HeaderNames {
 
   private val token = "someToken"
-  
+
   override lazy val authMicroService = mock[AuthMicroService]
   override lazy val saConnector = mock[SaConnector]
   override lazy val epayeConnector = mock[EpayeConnector]
   override lazy val ctConnector = mock[CtConnector]
   override lazy val vatConnector = mock[VatConnector]
-  
-  private val lottyRegime_empRef: EmpRef = EmpRef("123", "456")
-  private val lottyRegime_ctUtr: CtUtr = CtUtr("aCtUtr")
-  private val lottyRegime_vrn: Vrn = Vrn("someVrn")
-  private val lottyRegime_saUtr: SaUtr = SaUtr("aSaUtr")
+
+  private val lottyRegime_empRef = EmpRef("123", "456")
+  private val lottyRegime_ctUtr = CtUtr("aCtUtr")
+  private val lottyRegime_vrn = Vrn("someVrn")
+  private val lottyRegime_saUtr = SaUtr("aSaUtr")
 
   override protected def beforeEach(testData: TestData) {
     reset(authMicroService)
@@ -51,7 +70,17 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
     )
 
     when(authMicroService.authority("/auth/oid/gfisher")).thenReturn(
-      Some(UserAuthority(id = "/auth/oid/gfisher", saUtr = Some(SaUtr("3333333333")), regimes = Regimes(sa = Some(URI.create("/sa/detail/3333333333"))))))
+      Some(
+        UserAuthority(
+          id = "/auth/oid/gfisher",
+          saUtr = Some(SaUtr("3333333333")),
+          regimes = Regimes(
+            sa = Some(URI.create("/sa/detail/3333333333")
+            )
+          )
+        )
+      )
+    )
 
     when(authMicroService.authority("/auth/oid/lottyRegimes")).thenReturn(
       Some(UserAuthority("/auth/oid/lottyRegimes",
@@ -95,7 +124,11 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
 
   "basic homepage test" should {
     "contain the user's first name in the response" in new WithApplication(FakeApplication()) {
-      val result = test(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"), "userId" -> encrypt("/auth/oid/gfisher"), "token" -> encrypt(token)))
+      val result = test(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/gfisher"),
+        "token" -> encrypt(token)))
 
       status(result) should equal(200)
       contentAsString(result) should include("3333333333")
@@ -106,12 +139,22 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
     "return Unauthorised if no Authority is returned from the Auth service" in new WithApplication(FakeApplication()) {
       when(authMicroService.authority("/auth/oid/gfisher")).thenReturn(None)
 
-      val result = test(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"), "userId" -> encrypt("/auth/oid/gfisher"), "token" -> encrypt(token)))
+      val result = test(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/gfisher"),
+        "token" -> encrypt(token)))
+
       status(result) should equal(401)
     }
 
     "return internal server error page if the Action throws an exception" in new WithApplication(FakeApplication()) {
-      val result = testThrowsException(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"), "userId" -> encrypt("/auth/oid/gfisher"), "token" -> encrypt(token)))
+      val result = testThrowsException(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/gfisher"),
+        "token" -> encrypt(token)))
+
       status(result) should equal(500)
       contentAsString(result) should include("java.lang.RuntimeException")
     }
@@ -119,13 +162,23 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
     "return internal server error page if the AuthMicroService throws an exception" in new WithApplication(FakeApplication()) {
       when(authMicroService.authority("/auth/oid/gfisher")).thenThrow(new RuntimeException("TEST"))
 
-      val result = test(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"), "userId" -> encrypt("/auth/oid/gfisher"), "token" -> encrypt(token)))
+      val result = test(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/gfisher"),
+        "token" -> encrypt(token)))
+
       status(result) should equal(500)
       contentAsString(result) should include("java.lang.RuntimeException")
     }
 
     "include the authorisation and request ids in the MDC" in new WithApplication(FakeApplication()) {
-      val result = testMdc(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"), "userId" -> encrypt("/auth/oid/gfisher"), "token" -> encrypt(token)))
+      val result = testMdc(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/gfisher"),
+        "token" -> encrypt(token)))
+
       status(result) should equal(200)
       val strings = contentAsString(result).split(" ")
       strings(0) should equal("/auth/oid/gfisher")
@@ -135,7 +188,13 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
     "redirect to the Tax Regime landing page if the user is logged in but not authorised for the requested Tax Regime" in new WithApplication(FakeApplication()) {
       when(authMicroService.authority("/auth/oid/bob")).thenReturn(
         Some(UserAuthority("bob", Regimes(sa = None, paye = Some(URI.create("/personal/paye/12345678"))), None)))
-      val result = testAuthorisation(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"), "userId" -> encrypt("/auth/oid/bob"), "token" -> encrypt(token)))
+      val result = testAuthorisation(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/bob"),
+        "token" -> encrypt(token))
+      )
+
       status(result) should equal(303)
     }
 
@@ -152,11 +211,8 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
           implicit request =>
 
             when(saConnector.root("/saDetailEndpoint")).thenReturn(SaJsonRoot(Map("link1" -> "http://sa/1")))
-
             when(ctConnector.root("/ctDetailEndpoint")).thenReturn(CtJsonRoot(Map("link1" -> "http://ct/1")))
-
             when(vatConnector.root("/vatDetailEndpoint")).thenReturn(VatJsonRoot(Map("link1" -> "http://vat/1")))
-
             when(epayeConnector.root("/epayeDetailEndpoint")).thenReturn(EpayeJsonRoot(EpayeLinks(Some("http://epaye/1"))))
 
             user.regimes should have(
@@ -168,8 +224,12 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
             Ok("dummy argument")
       }
 
-      val result = testRegimeRoots(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
-        "userId" -> encrypt("/auth/oid/lottyRegimes"), "token" -> encrypt(token)))
+      val result = testRegimeRoots(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/lottyRegimes"),
+        "token" -> encrypt(token)
+      ))
 
       status(result) should equal(200)
     }
@@ -181,11 +241,8 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
           implicit request =>
 
             when(saConnector.root("/saDetailEndpoint")).thenReturn(SaJsonRoot(Map("link1" -> "http://sa/1")))
-
             when(ctConnector.root("/ctDetailEndpoint")).thenReturn(CtJsonRoot(Map("link1" -> "http://ct/1")))
-
             when(vatConnector.root("/vatDetailEndpoint")).thenReturn(VatJsonRoot(Map("link1" -> "http://vat/1")))
-
             when(epayeConnector.root("/epayeDetailEndpoint")).thenReturn(EpayeJsonRoot(EpayeLinks(Some("http://epaye/1"))))
 
             user.regimes should have(
@@ -198,15 +255,16 @@ class AuthorisedForGovernmentGatewayActionSpec extends BaseSpec with MockitoSuga
       }
 
       when(saConnector.root("/saDetailEndpoint")).thenReturn(SaJsonRoot(Map("link1" -> "http://sa/2")))
-
       when(ctConnector.root("/ctDetailEndpoint")).thenReturn(CtJsonRoot(Map("link1" -> "http://ct/2")))
-
       when(vatConnector.root("/vatDetailEndpoint")).thenReturn(VatJsonRoot(Map("link1" -> "http://vat/2")))
-
       when(epayeConnector.root("/epayeDetailEndpoint")).thenReturn(EpayeJsonRoot(EpayeLinks(Some("http://epaye/2"))))
 
-      val result = testRegimeRoots(FakeRequest().withSession("sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
-        "userId" -> encrypt("/auth/oid/lottyRegimes"), "token" -> encrypt(token)))
+      val result = testRegimeRoots(FakeRequest().withSession(
+        "sessionId" -> encrypt(s"session-${UUID.randomUUID().toString}"),
+        lastRequestTimestampKey -> now().getMillis.toString,
+        "userId" -> encrypt("/auth/oid/lottyRegimes"),
+        "token" -> encrypt(token))
+      )
 
       status(result) should equal(200)
     }
