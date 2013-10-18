70e2a7aa339f2fab41cf37b206ca650ffaa3c5ff
Eric Vlaanderen <eric.vlaanderen@digital.cabinet-office.gov.uk>
2013-10-18 09:39:38 +0100

    [HMTB-1123] - Move encryption trait to ida

--- sa-prefs/app/uk/gov/hmrc/Encryption.scala
+++ sa-prefs/app/uk/gov/hmrc/Encryption.scala
@@ -1,31 +1,8 @@
 package uk.gov.hmrc
 
-import org.apache.commons.codec.binary.Base64
-import javax.crypto.spec.SecretKeySpec
-import uk.gov.hmrc.secure.{ SymmetricDecrypter, SymmetricEncrypter }
-import play.api.Play
 import org.joda.time.{ DateTimeZone, DateTime }
-import java.net.{ URLDecoder, URLEncoder }
-
-trait Encryption {
-
-  val encryptionKey: String
-
-  private lazy val encryptionKeyBytes = Base64.decodeBase64(encryptionKey.getBytes("UTF-8"))
-
-  private lazy val secretKey = new SecretKeySpec(encryptionKeyBytes, "AES")
-
-  private val encrypter = new SymmetricEncrypter
-
-  private val decrypter = new SymmetricDecrypter
-
-  def encrypt(id: String) = encrypter.encrypt(id, secretKey)
-
-  def decrypt(id: String): String = decrypter.decrypt(id, secretKey)
-
-  def decrypt(id: Option[String]): Option[String] = id.map(decrypt)
-
-}
+import java.net.URLDecoder
+import uk.gov.hmrc.secure.Encryption
 
 case class TokenExpiredException(token: String, time: Long) extends Exception(s"Token expired: $token. Timestamp: $time, Now: ${DateTime.now(DateTimeZone.UTC).getMillis}")
 
