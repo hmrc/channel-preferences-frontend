383ecff3d7e649ed404efded31b42bde4708f5bd
howardbranch <howard.branch@digital.cabinet-office.gov.uk>
2013-10-24 15:53:11 +0100

    Added a test for last login time and placed some text in a messages file

--- govuk-tax/modules/paye/test/controllers/paye/PayeHomeControllerSpec.scala
+++ govuk-tax/modules/paye/test/controllers/paye/PayeHomeControllerSpec.scala
@@ -15,6 +15,9 @@ import play.api.test.FakeApplication
 import uk.gov.hmrc.common.microservice.paye.domain.TaxCode
 import uk.gov.hmrc.common.microservice.MockMicroServicesForTests
 import concurrent.Future
+import org.joda.time.DateTime
+import uk.gov.hmrc.common.microservice.domain.User
+import org.jsoup.nodes.Document
 
 class PayeHomeControllerSpec
   extends PayeBaseSpec
@@ -35,17 +38,17 @@ class PayeHomeControllerSpec
   "The home method" should {
 
     "display the name for John Densmore" in new WithApplication(FakeApplication()) {
-      val content = requestHomeAction
+      val content = requestHomeAction()
       content should include("John Densmore")
     }
 
     "display the tax codes for John Densmore" in new WithApplication() {
-      val content = requestHomeAction
+      val content = requestHomeAction()
       content should include("430L")
     }
 
     "display the employments for John Densmore" in new WithApplication(FakeApplication()) {
-      val content = requestHomeAction
+      val content = requestHomeAction()
       content should include("Weyland-Yutani Corp")
       content should include("899")
       content should include("1212121")
@@ -54,14 +57,14 @@ class PayeHomeControllerSpec
     }
 
     "display employer ref when the employer name is missing" in new WithApplication(FakeApplication()) {
-      val content = requestHomeAction
+      val content = requestHomeAction()
       content should include("1212121")
     }
 
     "display recent transactions for John Densmore" in new WithApplication(FakeApplication()) {
       setupMocksForJohnDensmore(johnDensmoresTaxCodes, johnDensmoresEmployments, johnDensmoresBenefits, testTransactions, testTransactions)
 
-      val doc = Jsoup.parse(requestHomeAction)
+      val doc = Jsoup.parse(requestHomeAction())
       val recentChanges = doc.select(".overview__actions__done").text
       recentChanges should include(s"On 2 December 2012, you removed your company car benefit from Weyland-Yutani Corp. This is being processed and you will receive a new Tax Code within 2 days.")
       recentChanges should include(s"On 2 December 2012, you removed your company car benefit from Weyland-Yutani Corp. This has been processed and your new Tax Code is 430L. Weyland-Yutani Corp have been notified.")
@@ -75,24 +78,39 @@ class PayeHomeControllerSpec
     "display recent transactions for multiple benefit removal for John Densmore" in new WithApplication(FakeApplication()) {
       setupMocksForJohnDensmore(johnDensmoresTaxCodes, johnDensmoresEmployments, johnDensmoresBenefits, multiBenefitTransactions, multiBenefitTransactions)
 
-      val doc = Jsoup.parse(requestHomeAction)
+      val doc = Jsoup.parse(requestHomeAction())
       doc.select(".overview__actions__done").text should include(s"2 December 2012, you removed your company car and fuel benefit from Weyland-Yutani Corp.")
       doc.select(".overview__actions__done").text should include(s"2 December 2012, you removed your company car and fuel benefit from Weyland-Yutani Corp. This has been processed and your new Tax Code is 430L. Weyland-Yutani Corp have been notified.")
     }
 
     "display a message for John Densmore if there are no transactions" in new WithApplication(FakeApplication()) {
-      val doc = Jsoup.parse(requestHomeAction)
+      val doc = Jsoup.parse(requestHomeAction())
       doc.select(".no_actions") should not be empty
       doc.select(".no_actions").text should include("no changes")
     }
 
     "return the link to the list of benefits for John Densmore" in new WithApplication(FakeApplication()) {
-      val content = requestHomeAction
+      val content = requestHomeAction()
       content should include("Click here to see your benefits")
     }
 
-    def requestHomeAction: String = {
-      val result = Future.successful(controller.homeAction(FakeRequest())(johnDensmore))
+    "show the first login message " in new WithApplication(FakeApplication()) {
+      val content = requestHomeAction()
+      Jsoup.parse(content).select("#lastlogin").text shouldBe "John Densmore, this is the first time you have logged in"
+    }
+
+    "show the last login time message " in new WithApplication(FakeApplication()) {
+      val previousLoginTime = new DateTime(2013, 10, 23, 9, 34, 45)
+      val johnDensmoreWithPreviousLoginTime = johnDensmore.copy(userAuthority = johnDensmore.userAuthority.copy(previouslyLoggedInAt = Some(previousLoginTime)))
+      val content = requestHomeAction(johnDensmoreWithPreviousLoginTime)
+      private val jsoupedPage: Document = Jsoup.parse(content)
+      jsoupedPage.select("#lastlogin").text shouldBe "John Densmore, you last accessed your account on Wednesday 23 October, 2013 at 9:34AM"
+      jsoupedPage.select(".overview__last-login").text() shouldBe "Your last login was on Wednesday 23 October, 2013 at 9:34AM."
+    }
+
+
+    def requestHomeAction(user: User = johnDensmore): String = {
+      val result = Future.successful(controller.homeAction(FakeRequest())(user))
       status(result) should be(200)
       contentAsString(result)
     }
