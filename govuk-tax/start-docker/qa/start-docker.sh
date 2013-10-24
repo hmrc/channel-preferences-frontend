051a5490b201fbe362d1f7a36a7bc0fdc2cdddd2
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2013-10-24 12:52:38 +0100

    Changed start docker to execute play startup script directly, as it's already executable

--- govuk-tax/start-docker/qa/start-docker.sh
+++ govuk-tax/start-docker/qa/start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #echo "127.0.0.1 $(hostname)" > /etc/hosts
-exec sh -ex /src/govuk-tax-0.0.1-SNAPSHOT/bin/govuk-tax -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/govuk-tax-logger.xml -Dconfig.resource=/qa.conf
+exec /src/govuk-tax-0.0.1-SNAPSHOT/bin/govuk-tax -Dhttp.port=8080 -Dapplication.log=INFO -Dlogger.resource=/govuk-tax-logger.xml -Dconfig.resource=/qa.conf
