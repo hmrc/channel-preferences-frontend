9601c943f27d10edc3d1eba530bb24c61cdd92b1
Duncan Crawford <duncan@tandemconsulting.eu>
2013-10-28 12:39:37 +0000

    removal of LoggingActionWrapper

--- govuk-tax/modules/common/app/controllers/common/actions/LoggingActionWrapper.scala
+++ govuk-tax/modules/common/app/controllers/common/actions/LoggingActionWrapper.scala
@@ -3,40 +3,35 @@ package controllers.common.actions
 import uk.gov.hmrc.utils.DateConverter
 import config.DateTimeProvider
 import util.Success
+import controllers.common.HeaderNames
 
+object WithRequestLogging extends HeaderNames with MdcHelper with DateConverter {
 
-trait LoggingActionWrapper {
+  import play.api.mvc._
+  import java.text.SimpleDateFormat
+  import java.util.Date
+  import play.api.Logger
+  import scala.concurrent.ExecutionContext
 
-  import controllers.common.HeaderNames
+  import ExecutionContext.Implicits.global
 
-  object WithRequestLogging extends HeaderNames with MdcHelper with DateConverter {
+  private val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ZZZZ")
 
-    import play.api.mvc._
-    import java.text.SimpleDateFormat
-    import java.util.Date
-    import play.api.Logger
-    import scala.concurrent.ExecutionContext
+  def apply(action: Action[AnyContent]): Action[AnyContent] = Action.async {
+    request =>
+    {
+      val start = DateTimeProvider.now().getMillis
+      val startTime = format.format(new Date(start))
+      val mdc = fromMDC()
 
-    import ExecutionContext.Implicits.global
+      action(request).andThen { case Success(result) =>
+        val elapsedTime = DateTimeProvider.now().getMillis - start
 
-    private val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ZZZZ")
-
-    def apply(action: Action[AnyContent]): Action[AnyContent] = Action.async {
-      request =>
-        {
-          val start = DateTimeProvider.now().getMillis
-          val startTime = format.format(new Date(start))
-          val mdc = fromMDC()
-
-          action(request).andThen { case Success(result) =>
-            val elapsedTime = DateTimeProvider.now().getMillis - start
-
-            // Apache combined log format http://httpd.apache.org/docs/2.4/logs.html
-            Logger.info(s"${mdc.get(forwardedFor).getOrElse(request.remoteAddress)} ${mdc.get(requestId).getOrElse("-")} " +
-              s"${mdc.get(authorisation).getOrElse("-")} [$startTime] '${request.method} ${request.uri}' ${result.header.status} " +
-              s"- ${elapsedTime}ms '${request.headers.get("Referer").getOrElse("-")}' '${request.headers.get("User-Agent").getOrElse("-")}'")
-          }
-        }
+        // Apache combined log format http://httpd.apache.org/docs/2.4/logs.html
+        Logger.info(s"${mdc.get(forwardedFor).getOrElse(request.remoteAddress)} ${mdc.get(requestId).getOrElse("-")} " +
+          s"${mdc.get(authorisation).getOrElse("-")} [$startTime] '${request.method} ${request.uri}' ${result.header.status} " +
+          s"- ${elapsedTime}ms '${request.headers.get("Referer").getOrElse("-")}' '${request.headers.get("User-Agent").getOrElse("-")}'")
+      }
     }
   }
 }
