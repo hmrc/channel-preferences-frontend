01613e13a5f30f6f49ccbdc8fa13755d9b4374e9
Duncan Crawford <duncan@tandemconsulting.eu>
2013-10-30 17:20:49 +0000

    create object CookieEncryption

--- govuk-tax/modules/common/app/controllers/common/CookieEncryption.scala
+++ govuk-tax/modules/common/app/controllers/common/CookieEncryption.scala
@@ -4,7 +4,7 @@ import play.api.Play
 import controllers.common.service.Encryption
 
 trait CookieEncryption extends Encryption {
-
   override lazy val encryptionKey = Play.current.configuration.getString("cookie.encryption.key").get
-
 }
+
+object CookieEncryption extends CookieEncryption
