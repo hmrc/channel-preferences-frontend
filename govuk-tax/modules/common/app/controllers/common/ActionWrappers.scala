7812ced9c850dcd16f303be6d156a73b929c02bb
Duncan Crawford <duncan@tandemconsulting.eu>
2013-11-07 17:58:44 +0000

    delete ActionWrappers

--- govuk-tax/modules/common/app/controllers/common/ActionWrappers.scala
+++ govuk-tax/modules/common/app/controllers/common/ActionWrappers.scala
@@ -1,13 +1,6 @@
 package controllers.common
 
-import controllers.common.service._
 import com.google.common.net.HttpHeaders
-import uk.gov.hmrc.common.microservice.auth.domain.UserAuthority
-import uk.gov.hmrc.common.microservice.domain.RegimeRoots
-import uk.gov.hmrc.common.microservice.sa.domain.SaRoot
-import uk.gov.hmrc.common.microservice.vat.domain.VatRoot
-import uk.gov.hmrc.common.microservice.ct.domain.CtRoot
-import uk.gov.hmrc.common.microservice.epaye.domain.EpayeRoot
 
 trait HeaderNames {
   val requestId = "X-Request-ID"
@@ -17,34 +10,3 @@ trait HeaderNames {
 }
 
 object HeaderNames extends HeaderNames
-
-@deprecated("please use Actions", "24.10.13")
-trait ActionWrappers
-  extends Connectors
-  with Actions  {
-
-  override def regimeRoots(authority: UserAuthority): RegimeRoots = {
-
-    val regimes = authority.regimes
-    RegimeRoots(
-      paye = regimes.paye map {
-        uri => payeConnector.root(uri.toString)
-      },
-      sa = regimes.sa map {
-        uri => SaRoot(authority.saUtr.get, saConnector.root(uri.toString))
-      },
-      vat = regimes.vat map {
-        uri => VatRoot(authority.vrn.get, vatConnector.root(uri.toString))
-      },
-      epaye = regimes.epaye.map {
-        uri => EpayeRoot(authority.empRef.get, epayeConnector.root(uri.toString))
-      },
-      ct = regimes.ct.map {
-        uri => CtRoot(authority.ctUtr.get, ctConnector.root(uri.toString))
-      },
-      agent = regimes.agent.map {
-        uri => agentConnectorRoot.root(uri.toString)
-      }
-    )
-  }
-}
