f1d1d8d337fc4d0a06d510e00e902c526bf11c5c
Doug Clinton <doug@gsl.com>
2013-11-26 14:19:34 +0000

    I was falling foul of the cascading futures by having them generated within the for-comprehension. I have now pulled them out so that all the futures are kicked off before we map any of them.

--- govuk-tax/modules/agent/app/controllers/agent/AgentsRegimeRoots.scala
+++ govuk-tax/modules/agent/app/controllers/agent/AgentsRegimeRoots.scala
@@ -9,13 +9,14 @@ import uk.gov.hmrc.common.microservice.auth.domain.UserAuthority
 
 trait AgentsRegimeRoots extends RegimeRootBase {
   def regimeRoots(authority: UserAuthority)(implicit hc: HeaderCarrier): Future[RegimeRoots] = {
+    val roots = (payeRoot(authority),saRoot(authority),vatRoot(authority),epayeRoot(authority),ctRoot(authority),agentRoot(authority))
     for {
-      paye <- payeRoot(authority)
-      sa <- saRoot(authority)
-      vat <- vatRoot(authority)
-      epaye <- epayeRoot(authority)
-      ct <- ctRoot(authority)
-      agent <- agentRoot(authority)
+      paye <- roots._1
+      sa <- roots._2
+      vat <- roots._3
+      epaye <- roots._4
+      ct <- roots._5
+      agent <- roots._6
     } yield RegimeRoots(paye = paye, sa = sa, vat = vat, epaye = epaye, ct = ct, agent = agent)
   }
 }
--- govuk-tax/modules/sa/app/controllers/sa/SaRegimeRoots.scala
+++ govuk-tax/modules/sa/app/controllers/sa/SaRegimeRoots.scala
@@ -9,12 +9,13 @@ import uk.gov.hmrc.common.microservice.auth.domain.UserAuthority
 
 trait SaRegimeRoots extends RegimeRootBase {
   def regimeRoots(authority: UserAuthority)(implicit hc: HeaderCarrier): Future[RegimeRoots] = {
+    val roots = (saRoot(authority), vatRoot(authority), epayeRoot(authority), ctRoot(authority), agentRoot(authority))
     for {
-      sa <- saRoot(authority)
-      vat <- vatRoot(authority)
-      epaye <- epayeRoot(authority)
-      ct <- ctRoot(authority)
-      agent <- agentRoot(authority)
+      sa <- roots._1
+      vat <- roots._2
+      epaye <- roots._3
+      ct <- roots._4
+      agent <- roots._5
     } yield RegimeRoots(sa = sa, vat = vat, epaye = epaye, ct = ct, agent = agent)
   }
 }
