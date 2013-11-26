7c2442eced6202dd43e1799b8ac0ff68ea019c63
Doug Clinton <doug@gsl.com>
2013-11-26 14:19:34 +0000

    I was falling foul of the cascading futures by having them generated within the for-comprehension. I have now pulled them out so that all the futures are kicked off before we map any of them.

--- govuk-tax/modules/business-tax/app/controllers/bt/BusinessTaxRegimeRoots.scala
+++ govuk-tax/modules/business-tax/app/controllers/bt/BusinessTaxRegimeRoots.scala
@@ -8,11 +8,12 @@ import uk.gov.hmrc.common.microservice.domain.RegimeRoots
 
 trait BusinessTaxRegimeRoots extends RegimeRootBase {
   def regimeRoots(authority: UserAuthority)(implicit hc: HeaderCarrier): Future[RegimeRoots] = {
+    val roots = (saRoot(authority), vatRoot(authority), epayeRoot(authority), ctRoot(authority))
     for {
-      sa <- saRoot(authority)
-      vat <- vatRoot(authority)
-      epaye <- epayeRoot(authority)
-      ct <- ctRoot(authority)
+      sa <- roots._1
+      vat <- roots._2
+      epaye <- roots._3
+      ct <- roots._4
     } yield RegimeRoots(sa = sa, vat = vat, epaye = epaye, ct = ct)
   }
 }
--- govuk-tax/modules/common/app/controllers/common/AllRegimeRoots.scala
+++ govuk-tax/modules/common/app/controllers/common/AllRegimeRoots.scala
@@ -5,17 +5,19 @@ import scala.concurrent._
 import uk.gov.hmrc.common.microservice.domain.RegimeRoots
 import controllers.common.actions.HeaderCarrier
 import uk.gov.hmrc.common.microservice.auth.domain.UserAuthority
+import sun.management.resources.agent
 
 
 trait AllRegimeRoots extends RegimeRootBase {
   def regimeRoots(authority: UserAuthority)(implicit hc: HeaderCarrier): Future[RegimeRoots] = {
+    val roots = (payeRoot(authority),saRoot(authority),vatRoot(authority),epayeRoot(authority),ctRoot(authority),agentRoot(authority))
     for {
-      paye <- payeRoot(authority)
-      sa <- saRoot(authority)
-      vat <- vatRoot(authority)
-      epaye <- epayeRoot(authority)
-      ct <- ctRoot(authority)
-      agent <- agentRoot(authority)
+      paye <- roots._1
+      sa <- roots._2
+      vat <- roots._3
+      epaye <- roots._4
+      ct <- roots._5
+      agent <- roots._6
     } yield RegimeRoots(paye = paye, sa = sa, vat = vat, epaye = epaye, ct = ct, agent = agent)
   }
 }
