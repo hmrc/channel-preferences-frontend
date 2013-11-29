2e95d718d2cae412e1b6b71d74841e3437187e4f
Jakob Grunig <jakob.grunig@digital.cabinet-office.gov.uk>
2013-11-29 10:54:10 +0000

    Fixed links in other services page

--- govuk-tax/modules/business-tax/app/controllers/bt/OtherServicesController.scala
+++ govuk-tax/modules/business-tax/app/controllers/bt/OtherServicesController.scala
@@ -37,32 +37,46 @@ class OtherServicesController(governmentGatewayConnector: GovernmentGatewayConne
     }
   }
 
-  def links(profile: ProfileResponse): List[OtherServiceLink] = {
+  def links(profile: ProfileResponse): Option[List[OtherServiceLink]] = {
 
     profile.affinityGroup match {
       case uk.gov.hmrc.common.microservice.governmentgateway.AffinityGroupValue.INDIVIDUAL =>
-        profile.activeEnrolments map { enrolment => enrolment.toLowerCase } collect {
-          case "hmce-ecsl-org" => OtherServiceLink("hmceecslorgHref", "https://customs.hmrc.gov.uk/ecsl/httpssl/start.do", "otherservices.manageTaxes.link.hmceecslorg", true)
-          case "hmrc-eu-ref-org" => OtherServiceLink("hmceecslorgHref", PortalConfig.getDestinationUrl("manageTaxes.euvat"), "otherservices.manageTaxes.link.hmrceureforg", false, true)
-          case "hmce-vatrsl-org" => OtherServiceLink("hmcevatrslorgHref", "https://customs.hmrc.gov.uk/rcsl/httpssl/Home.do", "otherservices.manageTaxes.link.hmcevatrslorg", true)
-       }
+        Some(profile.activeEnrolments map {
+          enrolment => enrolment.toLowerCase
+        } collect {
+          case "hmce-ecsl-org" => OtherServiceLink("hmceecslorgHref", "https://customs.hmrc.gov.uk/ecsl/httpssl/start.do", "otherservices.manageTaxes.link.hmceecslorg")
+          case "hmrc-eu-ref-org" => OtherServiceLink("hmrceureforgHref", PortalConfig.getDestinationUrl("manageTaxes.euvat"), "otherservices.manageTaxes.link.hmrceureforg", true)
+          case "hmce-vatrsl-org" => OtherServiceLink("hmcevatrslorgHref", "https://customs.hmrc.gov.uk/rcsl/httpssl/Home.do", "otherservices.manageTaxes.link.hmcevatrslorg")
+        })
       case uk.gov.hmrc.common.microservice.governmentgateway.AffinityGroupValue.ORGANISATION =>
-        profile.activeEnrolments map { enrolment => enrolment.toLowerCase } collect {
-          case "hmce-ddes" => OtherServiceLink("hmceddesHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmceddes", true)
-          case "hmce-ebti-org" => OtherServiceLink("hmceebtiorgHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmceebtiorg", true)
-          case "hmrc-emcs-org" => OtherServiceLink("hmrcemcsorgHref", PortalConfig.getDestinationUrl("manageTaxes.emcs"), "otherservices.manageTaxes.link.hmrcemcsorg", false, true)
-          case "hmrc-ics-org" => OtherServiceLink("hmrcicsorgHref", PortalConfig.getDestinationUrl("manageTaxes.ics"), "otherservices.manageTaxes.link.hmrcicsorg", false, true)
-          case "hmrc-mgd-org" => OtherServiceLink("hmrcmgdorgHref", PortalConfig.getDestinationUrl("manageTaxes.machinegames"), "otherservices.manageTaxes.link.hmrcmgdorg", false, true)
-          case "hmce-ncts-org" => OtherServiceLink("hmcenctsorgHref", "https://customs.hmrc.gov.uk/nctsPortalWebApp/ncts.portal?_nfpb=true&pageLabel=httpssIPageOnlineServicesAppNCTS_Home", "otherservices.manageTaxes.link.hmcenctsorg", true)
-          case "hmce-nes" => OtherServiceLink("hmcenesHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcenes", true)
-          case "hmce-ro" => { OtherServiceLink("hmcero1Href", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcero1", true)
-                              OtherServiceLink("hmcero2Href", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcero2", true) }
-          case "hmce-to" => OtherServiceLink("hmcetoHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmceto", true)
-          case "hmce-ecsl-org" => OtherServiceLink("hmceecslorgHref", "https://customs.hmrc.gov.uk/ecsl/httpssl/start.do", "otherservices.manageTaxes.link.hmceecslorg", true)
-          case "hmrc-eu-ref-org" => OtherServiceLink("hmceecslorgHref", PortalConfig.getDestinationUrl("manageTaxes.euvat"), "otherservices.manageTaxes.link.hmrceureforg", false, true)
-          case "hmce-vatrsl-org" => OtherServiceLink("hmcevatrslorgHref", "https://customs.hmrc.gov.uk/rcsl/httpssl/Home.do", "otherservices.manageTaxes.link.hmcevatrslorg", true)
+        val enrolments = profile.activeEnrolments map {
+          enrolment => enrolment.toLowerCase
         }
-      case _ => List.empty
+        val links = enrolments collect {
+          case "hmce-ddes" => OtherServiceLink("hmceddesHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmceddes")
+          case "hmce-ebti-org" => OtherServiceLink("hmceebtiorgHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmceebtiorg")
+          case "hmrc-emcs-org" => OtherServiceLink("hmrcemcsorgHref", PortalConfig.getDestinationUrl("manageTaxes.emcs"), "otherservices.manageTaxes.link.hmrcemcsorg", true)
+          case "hmrc-ics-org" => OtherServiceLink("hmrcicsorgHref", PortalConfig.getDestinationUrl("manageTaxes.ics"), "otherservices.manageTaxes.link.hmrcicsorg", true)
+          case "hmrc-mgd-org" => OtherServiceLink("hmrcmgdorgHref", PortalConfig.getDestinationUrl("manageTaxes.machinegames"), "otherservices.manageTaxes.link.hmrcmgdorg", true)
+          case "hmce-ncts-org" => OtherServiceLink("hmcenctsorgHref", "https://customs.hmrc.gov.uk/nctsPortalWebApp/ncts.portal?_nfpb=true&pageLabel=httpssIPageOnlineServicesAppNCTS_Home", "otherservices.manageTaxes.link.hmcenctsorg")
+          case "hmce-nes" => OtherServiceLink("hmcenesHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcenes")
+          case "hmce-to" => OtherServiceLink("hmcetoHref", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmceto")
+          case "hmce-ecsl-org" => OtherServiceLink("hmceecslorgHref", "https://customs.hmrc.gov.uk/ecsl/httpssl/start.do", "otherservices.manageTaxes.link.hmceecslorg")
+          case "hmrc-eu-ref-org" => OtherServiceLink("hmrceureforgHref", PortalConfig.getDestinationUrl("manageTaxes.euvat"), "otherservices.manageTaxes.link.hmrceureforg", true)
+          case "hmce-vatrsl-org" => OtherServiceLink("hmcevatrslorgHref", "https://customs.hmrc.gov.uk/rcsl/httpssl/Home.do", "otherservices.manageTaxes.link.hmcevatrslorg")
+        }
+
+        // and there is obviously the special case:
+        if (enrolments.contains("hmce-ro")) {
+          val hmceRo = List(
+            OtherServiceLink("hmcero1Href", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcero1"),
+            OtherServiceLink("hmcero2Href", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcero2"),
+            OtherServiceLink("hmcero3Href", "https://secure.hmce.gov.uk/ecom/login/index.html", "otherservices.manageTaxes.link.hmcero3")
+          )
+          Some(links ::: hmceRo)
+        } else Some(links)
+
+      case _ => None
     }
 
   }
@@ -70,4 +84,4 @@ class OtherServicesController(governmentGatewayConnector: GovernmentGatewayConne
 
 }
 
-case class OtherServiceLink(id: String, url: String, text: String, newWindow: Boolean = false, sso: Boolean = false)
+case class OtherServiceLink(id: String, url: String, text: String, sso: Boolean = false, newWindow: Boolean = true)
