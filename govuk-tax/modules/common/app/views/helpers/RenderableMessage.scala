6b7c1c1b6fb3ef84ed853fd880e5e8220465fe3f
Claudio Scandura <claudioscandura@HMRC030s-MacBook-Pro.local>
2013-12-04 11:31:41 +0000

    Removed deprecated classes LinkMessage and RenderableLinkMessage

--- govuk-tax/modules/common/app/views/helpers/RenderableMessage.scala
+++ govuk-tax/modules/common/app/views/helpers/RenderableMessage.scala
@@ -7,20 +7,6 @@ import views.helpers.RenderableMessageProperty.RenderableMessageProperty
 import java.text.{SimpleDateFormat, DateFormat}
 
 
-@deprecated("use Link")
-case class LinkMessage(href: String, text: String, id: String,
-                       newWindow: Boolean = false, sso: Boolean)
-
-object LinkMessage {
-
-  def internalLink(href: String, text: String, id: String) =
-    LinkMessage(href, text, id, newWindow = false, sso = false)
-
-  def portalLink(href: String, text: String, id: String) =
-    LinkMessage(href, text, id, newWindow = false, sso = true)
-
-}
-
 case class MoneyPounds(value: BigDecimal, decimalPlaces: Int = 2, roundUp: Boolean = false) {
 
   def isNegative = value < 0
@@ -47,8 +33,6 @@ trait RenderableMessage {
 object RenderableMessage {
   implicit def translateStrings(value: String): RenderableStringMessage = RenderableStringMessage(value)
 
-  implicit def translateLinks(link: LinkMessage): RenderableLinkMessage = RenderableLinkMessage(link)
-
   implicit def translateMoneyPounds(money: MoneyPounds): RenderableMoneyMessage = RenderableMoneyMessage(money)
 
   implicit def translateDate(date: LocalDate): RenderableDateMessage = RenderableDateMessage(date)
@@ -58,12 +42,6 @@ case class RenderableStringMessage(value: String) extends RenderableMessage {
   override def render: Html = Html(value)
 }
 
-@deprecated("use helper.link")
-case class RenderableLinkMessage(linkMessage: LinkMessage) extends RenderableMessage {
-  override def render: Html = {
-    link(linkMessage.id, linkMessage.href, linkMessage.text, linkMessage.newWindow, linkMessage.sso)
-  }
-}
 
 case class RenderableDateMessage(date: LocalDate)(implicit dateFormat: DateFormat = new SimpleDateFormat("d MMM yyy")) extends RenderableMessage {
   val formattedDate = dateFormat.format(date.toDate)
