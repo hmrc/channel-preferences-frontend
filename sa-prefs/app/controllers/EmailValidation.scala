0ad50e6ec945ce50d0b39bee4aaa88c5260a4c2f
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2013-12-10 12:00:52 +0000

    CS&MH Removed unused parameter from email validation end of flow pages.

--- sa-prefs/app/controllers/EmailValidation.scala
+++ sa-prefs/app/controllers/EmailValidation.scala
@@ -16,12 +16,12 @@ class EmailValidation extends Controller {
     token match {
       case regex(_) => {
         preferencesMicroService.updateEmailValidationStatus(token) match {
-          case EmailVerificationLinkResponse.OK => Ok(views.html.sa_printing_preference_verify_email(FrontEndConfig.portalHome))
-          case EmailVerificationLinkResponse.EXPIRED => Ok(views.html.sa_printing_preference_expired_email(FrontEndConfig.portalHome))
-          case EmailVerificationLinkResponse.ERROR => BadRequest(views.html.sa_printing_preference_verify_email_failed(FrontEndConfig.portalHome))
+          case EmailVerificationLinkResponse.OK => Ok(views.html.sa_printing_preference_verify_email())
+          case EmailVerificationLinkResponse.EXPIRED => Ok(views.html.sa_printing_preference_expired_email())
+          case EmailVerificationLinkResponse.ERROR => BadRequest(views.html.sa_printing_preference_verify_email_failed())
         }
       }
-      case _ => BadRequest(views.html.sa_printing_preference_verify_email_failed(FrontEndConfig.portalHome))
+      case _ => BadRequest(views.html.sa_printing_preference_verify_email_failed())
     }
   }
 }
