f107bbe0b74147b5e37b49a573a99256415c5bb5
Doug Clinton <doug@gsl.com>
2013-12-20 10:01:47 +0000

    Fixed the connection logging specs.

--- govuk-tax/modules/common/test/uk/gov/hmrc/common/microservice/ConnectionLoggingSpec.scala
+++ govuk-tax/modules/common/test/uk/gov/hmrc/common/microservice/ConnectionLoggingSpec.scala
@@ -6,32 +6,32 @@ class ConnectionLoggingSpec extends BaseSpec {
   "formatting nanoseconds" should {
     import ConnectionLogging.formatNs
 
-    "show 1 nanosecond as '1 ns'" in {
-      formatNs(1) shouldBe "1 ns"
+    "show 1 nanosecond as '1ns'" in {
+      formatNs(1) shouldBe "1ns"
     }
 
-    "show 1000 nanoseconds as '1 us'" in {
-      formatNs(1000) shouldBe "1.000 us"
+    "show 1000 nanoseconds as '1us'" in {
+      formatNs(1000) shouldBe "1.000us"
     }
 
-    "show 1050 nanoseconds as '1.050 us'" in {
-      formatNs(1050) shouldBe "1.050 us"
+    "show 1050 nanoseconds as '1.050us'" in {
+      formatNs(1050) shouldBe "1.050us"
     }
 
-    "show 1,000,000 nanoseconds as '1.000 ms'" in {
-      formatNs(1000000) shouldBe "1.000 ms"
+    "show 1,000,000 nanoseconds as '1.000ms'" in {
+      formatNs(1000000) shouldBe "1.000ms"
     }
 
-    "show 1,050,050 nanoseconds as '1.050 ms'" in {
-      formatNs(1050050) shouldBe "1.050 ms"
+    "show 1,050,050 nanoseconds as '1.050ms'" in {
+      formatNs(1050050) shouldBe "1.050ms"
     }
 
-    "show 1,000,000,000 nanoseconds as '1.000 s" in {
-      formatNs(1000000000) shouldBe "1.000 s"
+    "show 1,000,000,000 nanoseconds as '1.000s" in {
+      formatNs(1000000000) shouldBe "1.000s"
     }
 
-    "show 1,050,050,050 nanoseconds as '1.050 s" in {
-      formatNs(1050050050) shouldBe "1.050 s"
+    "show 1,050,050,050 nanoseconds as '1.050s" in {
+      formatNs(1050050050) shouldBe "1.050s"
     }
   }
 }
