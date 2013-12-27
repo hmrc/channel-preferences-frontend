d2f9b15383dee8da984438c79fdd9a03dba71dd1
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2013-12-27 11:33:28 +0000

    Upgraded version of metrics plugin.
    Upped version to 0.1.3.
    Re-instated metrics filter in application Global so we can get some metrics into graphite for analysing performance tests.

--- govuk-tax/modules/common/app/config/Global.scala
+++ govuk-tax/modules/common/app/config/Global.scala
@@ -1,50 +1,44 @@
 package config
 
-import play.api.GlobalSettings
-import play.api.mvc.RequestHeader
 
-//import play.api.{ Application, Logger }
-//import play.api.mvc._
-//import scala.concurrent.ExecutionContext.Implicits._
-//import java.text.SimpleDateFormat
-//import java.util.Date
-//import com.kenshoo.play.metrics.MetricsFilter
-//import com.codahale.metrics.graphite.{ GraphiteReporter, Graphite }
-//import java.net.InetSocketAddress
-//import com.codahale.metrics.{ MetricFilter, SharedMetricRegistries }
-//import java.util.concurrent.TimeUnit
-//import play.api.mvc.AsyncResult
+import play.api.Application
+import play.api.mvc._
+import com.kenshoo.play.metrics.MetricsFilter
+import com.codahale.metrics.graphite.{ GraphiteReporter, Graphite }
+import java.net.InetSocketAddress
+import com.codahale.metrics.{ MetricFilter, SharedMetricRegistries }
+import java.util.concurrent.TimeUnit
 
-//object Global extends WithFilters(MetricsFilter) {
-object Global extends GlobalSettings {
+object Global extends WithFilters(MetricsFilter) {
+//object Global extends GlobalSettings {
 
-//  override def onStart(app: Application) {
-//    val env = app.mode
-//    if (app.configuration.getBoolean("metrics.enabled").getOrElse(false) &&
-//      app.configuration.getBoolean(s"govuk-tax.$env.metrics.graphite.enabled").getOrElse(false)) {
-//      startGraphite(app)
-//    }
-//  }
+  override def onStart(app: Application) {
+    val env = app.mode
+    if (app.configuration.getBoolean("metrics.enabled").getOrElse(false) &&
+      app.configuration.getBoolean(s"govuk-tax.$env.metrics.graphite.enabled").getOrElse(false)) {
+      startGraphite(app)
+    }
+  }
 
-//  def startGraphite(app: Application) {
-//    val env = app.mode
-//
-//    val graphite = new Graphite(new InetSocketAddress(
-//      app.configuration.getString(s"govuk-tax.$env.metrics.graphite.host").getOrElse("graphite"),
-//      app.configuration.getInt(s"govuk-tax.$env.metrics.graphite.port").getOrElse(2003)))
-//
-//    val prefix = app.configuration.getString(s"govuk-tax.$env.metrics.graphite.prefix").getOrElse("tax")
-//
-//    val reporter = GraphiteReporter.forRegistry(
-//      SharedMetricRegistries.getOrCreate(app.configuration.getString("metrics.name").getOrElse("default")))
-//      .prefixedWith(s"$prefix.${java.net.InetAddress.getLocalHost.getHostName}")
-//      .convertRatesTo(TimeUnit.SECONDS)
-//      .convertDurationsTo(TimeUnit.MILLISECONDS)
-//      .filter(MetricFilter.ALL)
-//      .build(graphite)
-//
-//    reporter.start(app.configuration.getLong(s"govuk-tax.$env.metrics.graphite.interval").getOrElse(10L), TimeUnit.SECONDS)
-//  }
+  def startGraphite(app: Application) {
+    val env = app.mode
+
+    val graphite = new Graphite(new InetSocketAddress(
+      app.configuration.getString(s"govuk-tax.$env.metrics.graphite.host").getOrElse("graphite"),
+      app.configuration.getInt(s"govuk-tax.$env.metrics.graphite.port").getOrElse(2003)))
+
+    val prefix = app.configuration.getString(s"govuk-tax.$env.metrics.graphite.prefix").getOrElse("tax")
+
+    val reporter = GraphiteReporter.forRegistry(
+      SharedMetricRegistries.getOrCreate(app.configuration.getString("metrics.name").getOrElse("default")))
+      .prefixedWith(s"$prefix.${java.net.InetAddress.getLocalHost.getHostName}")
+      .convertRatesTo(TimeUnit.SECONDS)
+      .convertDurationsTo(TimeUnit.MILLISECONDS)
+      .filter(MetricFilter.ALL)
+      .build(graphite)
+
+    reporter.start(app.configuration.getLong(s"govuk-tax.$env.metrics.graphite.interval").getOrElse(10L), TimeUnit.SECONDS)
+  }
 
 
   // Play 2.0 doesn't support trailing slash: http://play.lighthouseapp.com/projects/82401/tickets/98
