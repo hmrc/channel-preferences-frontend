315f347b9a1285c910424868dc72ca8354ddb534
Duncan Crawford <duncan@tandemconsulting.eu>
2014-01-07 17:13:25 +0000

    metrics version to 3.0.1

--- govuk-tax/project/Dependencies.scala
+++ govuk-tax/project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val commonsIo = "commons-io" % "commons-io" % "2.4"
     val guava = "com.google.guava" % "guava" % "14.0.1"
     val playMetrics = "com.kenshoo" %% "metrics-play" % "0.1.3"
-    val metricsGraphite = "com.codahale.metrics" % "metrics-graphite" % "3.0.0"
+    val metricsGraphite = "com.codahale.metrics" % "metrics-graphite" % "3.0.1"
     val secureUtils = "uk.gov.hmrc" % "secure-utils" % "0.1.0-SNAPSHOT"
     val taxCore = "uk.gov.hmrc" %% "tax-core" % "0.2.0-SNAPSHOT"
   }
