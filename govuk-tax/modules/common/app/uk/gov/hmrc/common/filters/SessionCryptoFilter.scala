ccf78a30e78eaca3396a86e96b60ef75e5c5f3de
James Shade <james.shade@digital.cabinet-office.gov.uk>
2014-01-08 10:57:55 +0000

    James & Howy - Decrypt the cookie header

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/filters/SessionCryptoFilter.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/filters/SessionCryptoFilter.scala
@@ -4,28 +4,58 @@ import play.api.mvc._
 import scala.concurrent.Future
 import controllers.common.service.{Decrypter, Encrypter}
 import play.api.mvc.SimpleResult
+import play.api.http.HeaderNames.{COOKIE => CookieHeaderName}
 
 abstract class SessionCryptoFilter extends Filter with Encrypter with Decrypter {
 
   override def apply(next: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] = {
-    next(new RequestHeader {
-      def uri: String = rh.uri
 
-      def remoteAddress: String = rh.remoteAddress
+    val resultWithPlainSession = next(decryptSession(rh))
 
-      def queryString: Map[String, Seq[String]] = rh.queryString
+//    encryptSession(resultWithPlainSession)
+    resultWithPlainSession
+  }
 
-      def method: String = rh.method
+  private def decryptSession(rh: RequestHeader): RequestHeader = {
 
-      def headers: Headers = rh.headers
+    val cookies = rh.headers.getAll(CookieHeaderName).flatMap(Cookies.decode)
 
-      def path: String = rh.path
+    if (cookies.isEmpty) rh else rh.copy(headers = new Headers {
+      override protected val data: Seq[(String, Seq[String])] = {
 
-      def version: String = rh.version
 
-      def tags: Map[String, String] = rh.tags
+        val updatedCookies: Seq[Cookie] = cookies.map {
+          case session @ Cookie(Session.COOKIE_NAME, encryptedSession, _, _, _, _, _) => session.copy(value = decrypt(encryptedSession))
+          case other => other
+        }
 
-      def id: Long = rh.id
+        if (updatedCookies.isEmpty) rh.headers.toMap.toSeq
+        else (rh.headers.toMap + (CookieHeaderName -> Seq(Cookies.encode(updatedCookies)))).toSeq
+      }
     })
   }
+
+
+  private def encryptSession(resultWithPlainSession: Future[SimpleResult]): Future[SimpleResult] = ???
+
+//new RequestHeader {
+//      def uri: String = rh.uri
+//
+//      def remoteAddress: String = rh.remoteAddress
+//
+//      def queryString: Map[String, Seq[String]] = rh.queryString
+//
+//      def method: String = rh.method
+//
+//      def headers: Headers = rh.headers
+//
+//      def path: String = rh.path
+//
+//      def version: String = rh.version
+//
+//      def tags: Map[String, String] = rh.tags
+//
+//      def id: Long = rh.id
+//    })
+//  }
 }
--- govuk-tax/modules/common/test/uk/gov/hmrc/common/filters/SessionCryptoFilterSpec.scala
+++ govuk-tax/modules/common/test/uk/gov/hmrc/common/filters/SessionCryptoFilterSpec.scala
@@ -2,23 +2,28 @@ package uk.gov.hmrc.common.filters
 
 import uk.gov.hmrc.common.BaseSpec
 import org.scalatest.mock.MockitoSugar
-import play.api.mvc.{SimpleResult, RequestHeader}
+import play.api.mvc.{Session, Cookie, SimpleResult, RequestHeader}
 import scala.concurrent.Future
 import play.api.test.{WithApplication, FakeApplication, FakeRequest}
 import org.mockito.Mockito._
 import org.mockito.Matchers._
 import controllers.common.CookieCrypto
+import org.mockito.ArgumentCaptor
+import org.scalatest.OptionValues
 
-class SessionCryptoFilterSpec extends BaseSpec with MockitoSugar with CookieCrypto {
+class SessionCryptoFilterSpec extends BaseSpec with MockitoSugar with CookieCrypto with OptionValues {
 
   private trait Setup {
+
     val filter = new SessionCryptoFilter {
-      def decrypt(id: String): String = ???
 
-      def decrypt(id: Option[String]): Option[String] = ???
+      override def decrypt(id: String): String = ???
 
-      def encrypt(id: String): String = ???
+      override def decrypt(id: Option[String]): Option[String] = ???
+
+      override def encrypt(id: String): String = ???
     }
+
     val action = mock[(RequestHeader) => Future[SimpleResult]]
     val incomingRequest = FakeRequest()
     val outgoingResponse = Future.successful(mock[SimpleResult])
@@ -34,7 +39,29 @@ class SessionCryptoFilterSpec extends BaseSpec with MockitoSugar with CookieCryp
       }
     }
 
-    "do nothing with the session if it is empty" in new WithApplication(FakeApplication()) {
+    "decrypt the session" in new WithApplication(FakeApplication()) {
+      new Setup {
+        val encryptedCookie = Cookie(name = Session.COOKIE_NAME, value = "encryptedValue")
+        override val incomingRequest = FakeRequest().withCookies(encryptedCookie)
+        override val filter = new SessionCryptoFilter {
+          def decrypt(id: Option[String]): Option[String] = ???
+
+          def decrypt(id: String): String = id match {
+            case "encryptedValue" => "decryptedValue"
+            case somethingElse => fail(s"Unexpectedly tried to decrypt $somethingElse")
+          }
+
+          def encrypt(id: String): String = ???
+        }
+        filter(action)(incomingRequest)
+
+        val updatedRequest = ArgumentCaptor.forClass(classOf[RequestHeader])
+        verify(action).apply(updatedRequest.capture())
+        updatedRequest.getValue.cookies.get(Session.COOKIE_NAME).value should be (encryptedCookie.copy(value = "decryptedValue"))
+      }
+    }
+
+    "decrypt even an empty session" in new WithApplication(FakeApplication()) {
       new Setup {
         override val incomingRequest = FakeRequest().withSession()
         when(action.apply(any())).thenReturn(outgoingResponse)
@@ -43,28 +70,28 @@ class SessionCryptoFilterSpec extends BaseSpec with MockitoSugar with CookieCryp
       }
     }
 
-    "decrypt all keys and values in the session before passing them on to the next filter" in new WithApplication(FakeApplication()) {
+    "discard the session if it contains anything other than the encrypted entry" in new WithApplication(FakeApplication()) {
       new Setup {
-        override val incomingRequest = FakeRequest().withSession("encryptedKey" -> "encryptedValue")
-        override val filter = new SessionCryptoFilter {
-          def decrypt(id: Option[String]): Option[String] = ???
-
-          def decrypt(id: String): String = id match {
-            case "encryptedKey" => "decryptedKey"
-            case "encryptedValue" => "decryptedValue"
-            case somethingElse => fail(s"Unexpectedly tried to decrypt $somethingElse")
-          }
-
-          def encrypt(id: String): String = ???
-        }
-        filter(action)(incomingRequest)
-        verify(action).apply(incomingRequest.withSession("decryptedKey" -> "decryptedValue"))
+        pending
       }
     }
 
-    "discard the session if any entry cannot be decrypted" in {
-      pending
+    "discard the session if it cannot be decrypted" in new WithApplication(FakeApplication()) {
+      new Setup {
+        pending
+      }
+    }
+
+    "do nothing with the session result if it is empty" in new WithApplication(FakeApplication()) {
+      new Setup {
+        pending
+      }
+    }
+
+    "replace the values in the resulting session with a single encrypted value" in new WithApplication(FakeApplication()) {
+      new Setup {
+        pending
+      }
     }
   }
-
 }
