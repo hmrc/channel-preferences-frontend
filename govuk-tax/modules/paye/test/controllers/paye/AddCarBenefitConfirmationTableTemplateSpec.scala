e95df35047d2fdd89579a30371727068823d26a0
Luca Milanesio <luca.milanesio@digital.cabinet-office.gov.uk>
2014-01-09 15:54:44 +0000

    [HMTB-2571] Bug-fix on the employee contribution to the car
    If the user says "No" on the employee contribution form,
    then the table should say "None" as value instead of hiding
    the row as it was doing before.
    
    Possibly many other rows have the same issues, however we
    did not fix them now as they need to be double-checked with QA/BA
    
    Change-Id: If25295404b2b8befa37a2aeeedead58fafcad197

--- govuk-tax/modules/paye/test/controllers/paye/AddCarBenefitConfirmationTableTemplateSpec.scala
+++ govuk-tax/modules/paye/test/controllers/paye/AddCarBenefitConfirmationTableTemplateSpec.scala
@@ -1,22 +1,65 @@
 package controllers.paye
 
-import play.api.test.WithApplication
-import views.html.paye.add_car_benefit_confirmation_table
+import play.api.test.{FakeRequest, WithApplication, FakeApplication}
+import views.html.paye.{add_car_benefit_review, add_car_benefit_confirmation_table}
 import org.jsoup.Jsoup
 import play.api.test.Helpers._
-import play.api.test.FakeApplication
 import scala.collection.JavaConversions
 import JavaConversions._
 import play.api.i18n.Messages
+import play.api.mvc.Content
+import uk.gov.hmrc.common.microservice.paye.domain.AddCarBenefitConfirmationData
+import scala.Some
 
 
 class AddCarBenefitConfirmationTableTemplateSpec extends PayeBaseSpec with MockedTaxYearSupport {
+
+  def documentOf(content: Content) = Jsoup.parse(contentAsString(content))
+
+  "the review add car benefit table" should {
+    def addCarBenefitConfirmationDataForEmployeeContribution(employeeContribution: Option[Int]): AddCarBenefitConfirmationData = {
+      AddCarBenefitConfirmationData(
+        "", LocalDate.apply(), 0, "", None, None, None, None,
+        employeeContribution, None)
+    }
+
+    "display the contribution to the price of the car when entered" in new WithApplication(FakeApplication()) {
+      // given
+      val confirmationData = addCarBenefitConfirmationDataForEmployeeContribution(Some(1234))
+
+      // when
+      val doc = documentOf(add_car_benefit_review(
+        confirmationData, johnDensmore, "", 0, 0)(FakeRequest()))
+
+      // then
+      withClue("row with id=employee_contributions") {
+        doc.select("#employee_contributions") should not be (empty)
+        doc.select("#employee_contributions td").text should be("£1,234")
+      }
+    }
+
+    "display a \"not selected\" on the contribution to the price of the car when omitted" in new WithApplication(FakeApplication()) {
+      // given
+      val confirmationData = addCarBenefitConfirmationDataForEmployeeContribution(None)
+
+
+      // when
+      val doc = documentOf(add_car_benefit_review(
+        confirmationData, johnDensmore, "", 0, 0)(FakeRequest()))
+
+      // then
+      withClue("row with id=employee_contributions") {
+        doc.select("#employee_contributions") should not be (empty)
+        doc.select("#employee_contributions td").text should be(Messages("paye.add_car_benefit_review.none"))
+      }
+    }
+  }
 
   "add car benefit confirmation table template" should {
 
     implicit val user = johnDensmore
 
-    val data =johnDensmoresNewCarData
+    val data = johnDensmoresNewCarData
 
     "render a table summarising all the values added" in new WithApplication(FakeApplication()) {
       val doc = Jsoup.parse(contentAsString(add_car_benefit_confirmation_table(data)))
@@ -38,53 +81,35 @@ class AddCarBenefitConfirmationTableTemplateSpec extends PayeBaseSpec with Mocke
     }
 
     "not render the row containing the co2Figure value if not defined" in new WithApplication(FakeApplication()) {
-      val doc = Jsoup.parse(contentAsString(add_car_benefit_confirmation_table(data.copy(co2Figure = None))))
+      val doc = documentOf(add_car_benefit_confirmation_table(data.copy(co2Figure = None)))
 
       doc.getElementById("new-company-car-details") should bePresent
 
-      doc.getElementsByTag("th") should have size 7
       doc.getElementsByTag("th").toList.map(_.text) should not contain (Messages("paye.add_car_benefit_review.emissions"))
-      doc.getElementsByTag("td") should have size 7
     }
 
     "not render the row containing the engineCapacity value if not defined" in new WithApplication(FakeApplication()) {
-      val doc = Jsoup.parse(contentAsString(add_car_benefit_confirmation_table(data.copy(engineCapacity = None))))
+      val doc = documentOf(add_car_benefit_confirmation_table(data.copy(engineCapacity = None)))
 
       doc.getElementById("new-company-car-details") should bePresent
 
-      doc.getElementsByTag("th") should have size 7
       doc.getElementsByTag("th").toList.map(_.text) should not contain (Messages("paye.add_car_benefit_review.engine_capacity"))
-      doc.getElementsByTag("td") should have size 7
-    }
-
-    "not render the row containing the employeeContributions value if not defined" in new WithApplication(FakeApplication()) {
-      val doc = Jsoup.parse(contentAsString(add_car_benefit_confirmation_table(data.copy(employeeContributions = None))))
-
-      doc.getElementById("new-company-car-details") should bePresent
-
-      doc.getElementsByTag("th") should have size 7
-      doc.getElementsByTag("th").toList.map(_.text) should not contain (Messages("paye.add_car_benefit_review.employee_contributions"))
-      doc.getElementsByTag("td") should have size 7
     }
 
     "not render the row containing the dateRegistered value if not defined" in new WithApplication(FakeApplication()) {
-      val doc = Jsoup.parse(contentAsString(add_car_benefit_confirmation_table(data.copy(dateRegistered = None))))
+      val doc = documentOf(add_car_benefit_confirmation_table(data.copy(dateRegistered = None)))
 
       doc.getElementById("new-company-car-details") should bePresent
 
-      doc.getElementsByTag("th") should have size 7
       doc.getElementsByTag("th").toList.map(_.text) should not contain (Messages("paye.add_car_benefit_review.date_registered"))
-      doc.getElementsByTag("td") should have size 7
     }
 
     "not render the row containing the employerPayFuel value if not defined" in new WithApplication(FakeApplication()) {
-      val doc = Jsoup.parse(contentAsString(add_car_benefit_confirmation_table(data.copy(employerPayFuel = None))))
+      val doc = documentOf(add_car_benefit_confirmation_table(data.copy(employerPayFuel = None)))
 
       doc.getElementById("new-company-car-details") should bePresent
 
-      doc.getElementsByTag("th") should have size 7
       doc.getElementsByTag("th").toList.map(_.text) should not contain (Messages("paye.add_car_benefit_review.employer_pay_fuel"))
-      doc.getElementsByTag("td") should have size 7
     }
   }
 
