72f6eb6e22f1cf0f5aff19129ddbd95bcd142e11
Anthony Munene <munene.anthony@gmail.com>
2014-01-09 16:28:55 +0000

    HMTB-2541 add contact page and form

---
+++ govuk-tax/modules/common/app/controllers/common/ContactController.scala
@@ -0,0 +1,64 @@
+package controllers.common
+
+
+import controllers.common.actions.Actions
+import uk.gov.hmrc.common.microservice.auth.AuthConnector
+import uk.gov.hmrc.common.microservice.audit.AuditConnector
+import controllers.common.service.Connectors
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.json._
+import play.api.mvc.Request
+
+
+class ContactController(override val auditConnector: AuditConnector)(implicit override val authConnector: AuthConnector)
+  extends BaseController
+  with Actions
+  with AllRegimeRoots {
+
+  def this() = this(Connectors.auditConnector)(Connectors.authConnector)
+
+  val form = Form[ContactForm](
+    mapping(
+      "contact-name" -> text.verifying("error.common.problem_report.name_mandatory", action => !action.isEmpty),
+      "contact-comments" -> text.verifying("error.common.comments_mandatory", action => !action.isEmpty),
+      "contact-email" -> email,
+      "isJavascript" -> boolean
+    )(ContactForm.apply)(ContactForm.unapply)
+  )
+  def index = WithNewSessionTimeout(UnauthorisedAction{
+    implicit request =>
+    Ok(views.html.contact())
+  })
+
+  def submit = WithNewSessionTimeout(UnauthorisedAction {
+    implicit request => {
+      form.bindFromRequest.fold(
+        error => {
+          if (!error.data.getOrElse("isJavascript", "true").toBoolean) {
+            responseForNoJsBrowsers(true)
+          } else {
+            BadRequest(Json.toJson(Map("status" -> "ERROR")))
+          }
+        },
+        problemReport => {
+          // TODO, Store here the feedback
+
+          if (!problemReport.isJavascript) {
+            responseForNoJsBrowsers(false)
+          } else {
+            Ok(Json.toJson(
+              Map("status" -> "OK",
+                "message" -> "<h2 id=\"feedback-thank-you-header\">Thank you</h2> <p>Your comments will be reviewed by our customer support team.</p>"
+              )
+            ))
+          }
+        })
+    }
+  })
+
+  private def responseForNoJsBrowsers(hasErrors: Boolean)(implicit request: Request[AnyRef]) = Ok(views.html.problem_reports_confirmation(hasErrors, request.headers.get("referer").getOrElse("/home")))
+
+}
+
+case class ContactForm(contactName: String, contactEmail: String, contactComments: String, isJavascript: Boolean)
---
+++ govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
@@ -0,0 +1,87 @@
+package controllers.common
+
+import uk.gov.hmrc.common.BaseSpec
+import play.api.test.{FakeRequest, WithApplication}
+import play.api.test.Helpers._
+import play.api.test.FakeApplication
+import org.jsoup.Jsoup
+
+class ContactControllerSpec extends BaseSpec {
+  "Submitting Contact form" should {
+    "return 200 and a valid json for a valid request and js is enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "John Densmore", "contact-comments" -> "some comments" , "contact-email" -> "name@mail.com", "isJavascript" -> "true"))
+
+      status(result) should be(200)
+
+      contentAsJson(result).\("status").as[String] shouldBe "OK"
+      contentAsJson(result).\("message").as[String] shouldBe "<h2 id=\"feedback-thank-you-header\">Thank you</h2> <p>Your comments will be reviewed by our customer support team.</p>"
+    }
+
+    "return 200 and a valid html page for a valid request and js is not enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "John Densmore", "contact-comments" -> "some comments" , "contact-email" -> "name@mail.com", "isJavascript" -> "false"))
+
+      status(result) should be(200)
+
+      val document = Jsoup.parse(contentAsString(result))
+      document.getElementById("report-confirmation") should not be null
+    }
+
+    "return 200 and a valid html page for an invalid name and js is not enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "", "contact-comments" -> "some comments" , "contact-email" -> "name@mail.com", "isJavascript" -> "false"))
+
+      status(result) should be(200)
+
+      val document = Jsoup.parse(contentAsString(result))
+      document.getElementById("report-confirmation-no-data") should not be null
+    }
+
+
+    "return 200 and a valid html page for an invalid email and js is not enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "John Densmore", "contact-comments" -> "some comments" , "contact-email" -> "", "isJavascript" -> "false"))
+
+      status(result) should be(200)
+
+      val document = Jsoup.parse(contentAsString(result))
+      document.getElementById("report-confirmation-no-data") should not be null
+    }
+
+    "return 400 and a valid json for an invalid name and js is enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "", "contact-comments" -> "some comments" , "contact-email" -> "name@mail.com", "isJavascript" -> "true"))
+
+      status(result) should be(400)
+
+      contentAsJson(result).\("status").as[String] shouldBe "ERROR"
+    }
+
+    "return 400 and a valid json for an invalid email and js is enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "John Densmore", "contact-comments" -> "some comments" , "contact-email" -> "", "isJavascript" -> "true"))
+
+      status(result) should be(400)
+
+      contentAsJson(result).\("status").as[String] shouldBe "ERROR"
+    }
+    "return 400 and a valid json for an invalid comment and js is enabled" in new WithApplication(FakeApplication()){
+      val controller = new ContactController()
+
+      val result = controller.submit()(FakeRequest().withFormUrlEncodedBody("contact-name" -> "John Densmore", "contact-comments" -> "" , "contact-email" -> "name@mail.com", "isJavascript" -> "true"))
+
+      status(result) should be(400)
+
+      contentAsJson(result).\("status").as[String] shouldBe "ERROR"
+    }
+  }
+
+
+}
