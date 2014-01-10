54f568aad0f7f3e3ac0e9714dc12bf12985f22f2
Howy Perrin <howard.perrin@digital.cabinet-office.gov.uk>
2014-01-10 15:56:46 +0000

    Remove redundant sa-prefs sbt/environment files

--- govuk-tax/modules/sa-prefs/start-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-exec /src/sa-prefs-0.0.1-SNAPSHOT/bin/sa-prefs \
-  -Dhttp.port=8080 \
-  -Dapplication.log=INFO \
-  -Dlogger.resource=/sa-prefs-logger.xml \
-  -Dconfig.resource=/application.conf \
-  $HMRC_CONFIG
-
