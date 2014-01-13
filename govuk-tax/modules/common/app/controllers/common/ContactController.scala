132af93c2c2a8691930e503d15e2f3ec358924a1
Jakob Grunig <jakob.grunig@digital.cabinet-office.gov.uk>
2014-01-13 20:50:58 +0000

    Showing contact page only to authenticated users

--- govuk-tax/modules/common/app/controllers/common/ContactController.scala
+++ govuk-tax/modules/common/app/controllers/common/ContactController.scala
@@ -1,13 +1,18 @@
 package controllers.common
 
 
-import controllers.common.actions.{HeaderCarrier, Actions}
+import controllers.common.actions.Actions
 import uk.gov.hmrc.common.microservice.auth.AuthConnector
 import uk.gov.hmrc.common.microservice.audit.AuditConnector
 import controllers.common.service.Connectors
 import play.api.data._
 import play.api.data.Forms._
+import play.api.mvc.Results._
 import uk.gov.hmrc.common.microservice.deskpro.{Ticket, HmrcDeskproConnector}
+import play.api.mvc.{AnyContent, Request}
+import scala.concurrent.Future
+import play.api.i18n.Messages
+import uk.gov.hmrc.common.microservice.domain.User
 
 
 class ContactController(override val auditConnector: AuditConnector, hmrcDeskproConnector: HmrcDeskproConnector)(implicit override val authConnector: AuthConnector)
@@ -26,40 +31,47 @@ class ContactController(override val auditConnector: AuditConnector, hmrcDeskpro
       "referer" -> text
     )(ContactForm.apply)(ContactForm.unapply)
   )
-  def index = WithNewSessionTimeout(UnauthorisedAction{
-    implicit request =>
-    Ok(views.html.contact(form.fill(ContactForm(request.headers.get("Referer").getOrElse("n/a")))))
-  })
 
-  def submit = WithNewSessionTimeout(UnauthorisedAction {
-    implicit request => {
-      form.bindFromRequest()(request).fold(
-        error => {
-            BadRequest(views.html.contact(error))
-        },
-        data => {
-          implicit val hc = HeaderCarrier(request)
-          hmrcDeskproConnector.createTicket(Ticket(
-            data.contactName,
-            data.contactEmail,
-            "Contact form submission",
-            data.contactComments,
-            data.referer,
-            if(data.isJavascript) "Y" else "N",
-            request.headers.get("User-Agent").getOrElse("n/a"),
-            hc.userId.getOrElse("n/a"),
-            "paye|biztax",
-            hc.sessionId.getOrElse("n/a")))
+  def index = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)( {
+    user => request => renderIndex(user, request)
+  }))
 
-          Redirect(routes.ContactController.thanks())
+  private[common] def renderIndex(implicit user: User, request: Request[AnyRef]) =  Ok(views.html.contact(form.fill(ContactForm(request.headers.get("Referer").getOrElse("n/a")))))
 
-        })
-    }
-  })
+  def submit = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)( {
+    user => request => doSubmit(user, request)
+  }))
 
-  def thanks = WithNewSessionTimeout(UnauthorisedAction {
-    implicit request => Ok(views.html.contact_confirmation())
-  })
+  private[common] def doSubmit(implicit user: User, request: Request[AnyRef]) = {
+    form.bindFromRequest()(request).fold(
+      error => {
+        BadRequest(views.html.contact(error))
+      },
+      data => {
+        hmrcDeskproConnector.createTicket(createTicket(data))
+        Redirect(routes.ContactController.thanks())
+
+      })
+  }
+
+
+  def createTicket(data: ContactForm)(implicit user: User, request: Request[AnyRef]): Ticket = {
+    Ticket(
+      data.contactName,
+      data.contactEmail,
+      "Contact form submission",
+      data.contactComments,
+      data.referer,
+      if (data.isJavascript) "Y" else "N",
+      request.headers.get("User-Agent").getOrElse("n/a"),
+      hc.userId.getOrElse("n/a"),
+      if (user.regimes.paye.isDefined) "paye" else "biztax",
+      hc.sessionId.getOrElse("n/a"))
+  }
+
+  def thanks = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)( {
+    implicit user => implicit request => Ok(views.html.contact_confirmation())
+  }))
 
 }
 
@@ -67,4 +79,12 @@ case class ContactForm(contactName: String, contactEmail: String, contactComment
 
 object ContactForm {
   def apply(referer: String): ContactForm = ContactForm("", "", "", false, referer)
+}
+
+object AuthenticatedOr404Provider extends AuthenticationProvider{
+  def notFoundPage(request: Request[AnyContent]) = Future.successful(Right(NotFound(views.html.global_error(Messages("global.error.heading"), "The requested resource doesn't seem to exist: " + request.path))))
+  def handleNotAuthenticated(request: Request[AnyContent], redirectToOrigin: Boolean) = {
+    case UserCredentials(None, _) =>  notFoundPage(request)
+    case UserCredentials(Some(_), None) =>  notFoundPage(request)
+  }
 }
--- govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
+++ govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
@@ -3,10 +3,70 @@ package controllers.common
 import uk.gov.hmrc.common.BaseSpec
 import play.api.test.{FakeRequest, WithApplication}
 import play.api.test.Helpers._
-import play.api.test.FakeApplication
 import org.jsoup.Jsoup
+import uk.gov.hmrc.common.microservice.audit.AuditConnector
+import uk.gov.hmrc.common.microservice.deskpro.HmrcDeskproConnector
+import uk.gov.hmrc.common.microservice.auth.AuthConnector
+import play.api.test.FakeApplication
+import org.scalatest.mock.MockitoSugar
+import uk.gov.hmrc.common.microservice.domain.{RegimeRoots, User}
+import uk.gov.hmrc.common.microservice.auth.domain.{CreationAndLastModifiedDetail, Accounts, Credentials, Authority}
+import uk.gov.hmrc.utils.DateTimeUtils
 
-class ContactControllerSpec extends BaseSpec {
+class ContactControllerSpec extends BaseSpec with MockitoSugar {
 
+  private abstract class WithContactController extends WithApplication(FakeApplication()) {
+    lazy val controller = new ContactController(mock[AuditConnector], mock[HmrcDeskproConnector])(mock[AuthConnector])
+  }
+
+  "Contact page" should {
+
+    "return 404 if not authenticated" in  new WithContactController {
+      private val index = controller.index(FakeRequest().withSession(SessionKeys.lastRequestTimestamp -> DateTimeUtils.now.getMillis.toString))
+      status(index) shouldBe 404
+    }
+
+    "inject the referer if available" in new WithContactController {
+
+      private val index = controller.renderIndex(FakeRequest().withHeaders("Referer" -> "SomeURL"))
+      val page = Jsoup.parse(contentAsString(index))
+
+      page.getElementById("referer").attr("value") shouldBe "SomeURL"
+
+    }
+
+    "verify that all fields are set" in new WithContactController {
+
+      val user = User("123", Authority("/auth/oid/123", Credentials(), Accounts(), None, None, CreationAndLastModifiedDetail()), RegimeRoots())
+      val submit = controller.doSubmit(user, FakeRequest().withFormUrlEncodedBody("contact-name" -> "", "contact-email" -> "", "contact-comments" -> ""))
+      val page = Jsoup.parse(contentAsString(submit))
+
+      status(submit) shouldBe 400
+
+      page.getElementsByClass("error-notification").size() shouldBe 3
+      page.getElementsByClass("error-notification").get(0).text() shouldBe "Please provide your name."
+      page.getElementsByClass("error-notification").get(1).text() shouldBe "Valid email required"
+      page.getElementsByClass("error-notification").get(2).text() shouldBe "Please provide details"
+    }
+
+    "create a ticket when the form is correct" in new WithContactController {
+      val request = FakeRequest().withHeaders("User-Agent" -> "UA").withSession("sessionId" -> "123")
+
+      val user = User("123", Authority("/auth/oid/123", Credentials(), Accounts(), None, None, CreationAndLastModifiedDetail()), RegimeRoots())
+      val ticket =  controller.createTicket(ContactForm("My Name", "my@email.com", "TP Rocks!", false, "SomeUrl"))(user, request)
+
+      ticket.name shouldBe "My Name"
+      ticket.email shouldBe "my@email.com"
+      ticket.message shouldBe "TP Rocks!"
+      ticket.javascriptEnabled shouldBe "N"
+      ticket.referrer shouldBe "SomeUrl"
+      ticket.userAgent shouldBe "UA"
+      ticket.sessionId shouldBe "123"
+      ticket.subject shouldBe "Contact form submission"
+      ticket.areaOfTax shouldBe "biztax"
+
+    }
+
+  }
 
 }
