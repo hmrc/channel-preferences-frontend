d95a6b0ee13e7d26266074fa14cf2a9ab5284726
Maria Rubio-Gonzalez <maria.rubio-gonzalez@digital.cabinet-office.gov.uk>
2014-01-14 12:11:40 +0000

    SE MR [HMTB-2541] Added ticket id to contact us confirmation page

--- govuk-tax/modules/common/app/controllers/common/ContactController.scala
+++ govuk-tax/modules/common/app/controllers/common/ContactController.scala
@@ -1,7 +1,7 @@
 package controllers.common
 
 
-import controllers.common.actions.Actions
+import controllers.common.actions.{HeaderCarrier, Actions}
 import uk.gov.hmrc.common.microservice.auth.AuthConnector
 import uk.gov.hmrc.common.microservice.audit.AuditConnector
 import controllers.common.service.Connectors
@@ -13,14 +13,20 @@ import play.api.mvc.{AnyContent, Request}
 import scala.concurrent.Future
 import play.api.i18n.Messages
 import uk.gov.hmrc.common.microservice.domain.User
+import uk.gov.hmrc.common.microservice.keystore.KeyStoreConnector
 
 
-class ContactController(override val auditConnector: AuditConnector, hmrcDeskproConnector: HmrcDeskproConnector)(implicit override val authConnector: AuthConnector)
+class ContactController(override val auditConnector: AuditConnector, hmrcDeskproConnector: HmrcDeskproConnector, keyStoreConnector: KeyStoreConnector)(implicit override val authConnector: AuthConnector)
   extends BaseController
   with Actions
   with AllRegimeRoots {
 
-  def this() = this(Connectors.auditConnector, Connectors.hmrcDeskproConnector)(Connectors.authConnector)
+  val actionId: String = "confirmTicket"
+  val source: String = "tickets"
+  val formId: String = "FeedbackForm"
+  val ticketKey: String = "ticketId"
+
+  def this() = this(Connectors.auditConnector, Connectors.hmrcDeskproConnector, Connectors.keyStoreConnector)(Connectors.authConnector)
 
   val form = Form[ContactForm](
     mapping(
@@ -32,24 +38,31 @@ class ContactController(override val auditConnector: AuditConnector, hmrcDeskpro
     )(ContactForm.apply)(ContactForm.unapply)
   )
 
-  def index = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)( {
+  def index = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)({
     user => request => renderIndex(user, request)
   }))
 
-  private[common] def renderIndex(implicit user: User, request: Request[AnyRef]) =  Ok(views.html.contact(form.fill(ContactForm(request.headers.get("Referer").getOrElse("n/a")))))
+  private[common] def renderIndex(implicit user: User, request: Request[AnyRef]) = Ok(views.html.contact(form.fill(ContactForm(request.headers.get("Referer").getOrElse("n/a")))))
 
-  def submit = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)( {
+  def submit = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider).async({
     user => request => doSubmit(user, request)
   }))
+
+
 
   private[common] def doSubmit(implicit user: User, request: Request[AnyRef]) = {
     form.bindFromRequest()(request).fold(
       error => {
-        BadRequest(views.html.contact(error))
+        Future.successful(BadRequest(views.html.contact(error)))
       },
       data => {
-        hmrcDeskproConnector.createTicket(createTicket(data))
-        Redirect(routes.ContactController.thanks())
+        hmrcDeskproConnector.createTicket(createTicket(data)).flatMap {
+          ticket =>
+            val ticketId = ticket.map(_.ticket_id.toString).getOrElse("Unknown")
+            keyStoreConnector.addKeyStoreEntry[Map[String, String]](actionId, source, formId, Map(ticketKey -> ticketId)).map(
+              _ => Redirect(routes.ContactController.thanks())
+            )
+        }
 
       })
   }
@@ -69,10 +82,19 @@ class ContactController(override val auditConnector: AuditConnector, hmrcDeskpro
       hc.sessionId.getOrElse("n/a"))
   }
 
-  def thanks = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider)( {
-    implicit user => implicit request => Ok(views.html.contact_confirmation())
+  def thanks = WithNewSessionTimeout(AuthenticatedBy(AuthenticatedOr404Provider).async({
+    implicit user => implicit request => doThanks(user, request)
   }))
 
+
+  def doThanks(user: User, request: Request[AnyRef]) = {
+    implicit val hc = HeaderCarrier(request)
+    keyStoreConnector.getEntry[Map[String, String]](actionId, source, formId).map {
+      keyStoreData =>
+        val ticketId: String = keyStoreData.flatMap(_.get(ticketKey)).getOrElse("Unknown")
+        Ok(views.html.contact_confirmation(ticketId)(user, request))
+    }
+  }
 }
 
 case class ContactForm(contactName: String, contactEmail: String, contactComments: String, isJavascript: Boolean, referer: String)
@@ -88,8 +110,8 @@ object AuthenticatedOr404Provider extends AuthenticationProvider{
     Messages("global.error.pageNotFound404.message")))))
 
   def handleNotAuthenticated(request: Request[AnyContent], redirectToOrigin: Boolean) = {
-    case UserCredentials(None, _) =>  notFoundPage(request)
-    case UserCredentials(Some(_), None) =>  notFoundPage(request)
+    case UserCredentials(None, _) => notFoundPage(request)
+    case UserCredentials(Some(_), None) => notFoundPage(request)
   }
 
 }
--- govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
+++ govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
@@ -7,31 +7,69 @@ import org.jsoup.Jsoup
 import uk.gov.hmrc.common.microservice.audit.AuditConnector
 import uk.gov.hmrc.common.microservice.deskpro.HmrcDeskproConnector
 import uk.gov.hmrc.common.microservice.auth.AuthConnector
-import play.api.test.FakeApplication
 import org.scalatest.mock.MockitoSugar
-import uk.gov.hmrc.common.microservice.domain.{RegimeRoots, User}
-import uk.gov.hmrc.common.microservice.auth.domain.{CreationAndLastModifiedDetail, Accounts, Credentials, Authority}
+import uk.gov.hmrc.common.microservice.auth.domain.CreationAndLastModifiedDetail
 import uk.gov.hmrc.utils.DateTimeUtils
+import uk.gov.hmrc.common.microservice.keystore.KeyStoreConnector
+import org.mockito.Mockito._
+import org.mockito.Matchers.any
+import org.mockito.Matchers.{eq => meq}
+import scala.concurrent.Future
+import scala.concurrent.Future._
+import controllers.common.actions.HeaderCarrier
 import uk.gov.hmrc.common.microservice.paye.domain.PayeRoot
+import uk.gov.hmrc.common.microservice.auth.domain.Accounts
+import uk.gov.hmrc.common.microservice.auth.domain.Authority
+import scala.Some
+import uk.gov.hmrc.common.microservice.auth.domain.Credentials
+import uk.gov.hmrc.common.microservice.domain.User
+import uk.gov.hmrc.common.microservice.domain.RegimeRoots
+import uk.gov.hmrc.common.microservice.deskpro.Ticket
+import play.api.test.FakeApplication
+import uk.gov.hmrc.common.microservice.deskpro.TicketId
 
 class ContactControllerSpec extends BaseSpec with MockitoSugar {
 
   private abstract class WithContactController extends WithApplication(FakeApplication()) {
-    lazy val controller = new ContactController(mock[AuditConnector], mock[HmrcDeskproConnector])(mock[AuthConnector])
+    lazy val deskProConnector: HmrcDeskproConnector = mock[HmrcDeskproConnector]
+    lazy val keyStoreConnector = mock[KeyStoreConnector]
+    lazy val controller = new ContactController(mock[AuditConnector], deskProConnector, keyStoreConnector)(mock[AuthConnector])
+    val user = {
+      val root = PayeRoot("nino", "mr", "John", None, "Densmore", "JD", "DOB", Map.empty, Map.empty, Map.empty)
+      User("123", Authority("/auth/oid/123", Credentials(), Accounts(), None, None, CreationAndLastModifiedDetail()), RegimeRoots(Some(root)))
+    }
   }
+
+  type StoredTicket = Map[String, String]
 
   "Contact page" should {
 
-    "return 404 if not authenticated" in  new WithContactController {
+
+    "redirect to the confirmation page when ticket is created" in new WithContactController {
+      when(deskProConnector.createTicket(any[Ticket])(any[HeaderCarrier])).thenReturn(Future.successful(Some(TicketId(123))))
+
+      import controller._
+
+
+      private val keyStoreData: StoredTicket= Map(ticketKey -> "123")
+      when(keyStoreConnector.addKeyStoreEntry[StoredTicket](meq(actionId), meq(source), meq(formId), meq(keyStoreData), meq(false))(any(classOf[Manifest[StoredTicket]]), any[HeaderCarrier])).thenReturn(successful(None))
+
+      val submit = controller.doSubmit(user, FakeRequest().withFormUrlEncodedBody("contact-name" -> "Foo", "contact-email" -> "foo@bar.com", "contact-comments" -> "it works", "isJavascript" -> "true", "referer" -> "mozilllaaaaa"))
+      val page = Jsoup.parse(contentAsString(submit))
+
+      status(submit) shouldBe 303
+      submit.header.headers("Location") shouldBe "/contact-hmrc/thanks"
+
+    }
+
+
+    "return 404 if not authenticated" in new WithContactController {
       private val index = controller.index(FakeRequest().withSession(SessionKeys.lastRequestTimestamp -> DateTimeUtils.now.getMillis.toString))
       status(index) shouldBe 404
     }
 
     "inject the referer if available" in new WithContactController {
-      val user = {
-        val root = PayeRoot("nino", "mr", "John", None, "Densmore", "JD", "DOB", Map.empty, Map.empty, Map.empty)
-        User("123", Authority("/auth/oid/123", Credentials(), Accounts(), None, None, CreationAndLastModifiedDetail()), RegimeRoots(Some(root)))
-      }
+
       private val index = controller.renderIndex(user, FakeRequest().withHeaders("Referer" -> "SomeURL"))
       val page = Jsoup.parse(contentAsString(index))
 
@@ -40,10 +78,7 @@ class ContactControllerSpec extends BaseSpec with MockitoSugar {
     }
 
     "verify that all fields are set" in new WithContactController {
-      val user = {
-        val root = PayeRoot("nino", "mr", "John", None, "Densmore", "JD", "DOB", Map.empty, Map.empty, Map.empty)
-        User("123", Authority("/auth/oid/123", Credentials(), Accounts(), None, None, CreationAndLastModifiedDetail()), RegimeRoots(Some(root)))
-      }
+
       val submit = controller.doSubmit(user, FakeRequest().withFormUrlEncodedBody("contact-name" -> "", "contact-email" -> "", "contact-comments" -> ""))
       val page = Jsoup.parse(contentAsString(submit))
 
@@ -56,10 +91,10 @@ class ContactControllerSpec extends BaseSpec with MockitoSugar {
     }
 
     "create a ticket when the form is correct" in new WithContactController {
+
       val request = FakeRequest().withHeaders("User-Agent" -> "UA").withSession("sessionId" -> "123")
 
-      val user = User("123", Authority("/auth/oid/123", Credentials(), Accounts(), None, None, CreationAndLastModifiedDetail()), RegimeRoots())
-      val ticket =  controller.createTicket(ContactForm("My Name", "my@email.com", "TP Rocks!", false, "SomeUrl"))(user, request)
+      val ticket = controller.createTicket(ContactForm("My Name", "my@email.com", "TP Rocks!", false, "SomeUrl"))(user, request)
 
       ticket.name shouldBe "My Name"
       ticket.email shouldBe "my@email.com"
@@ -69,10 +104,28 @@ class ContactControllerSpec extends BaseSpec with MockitoSugar {
       ticket.userAgent shouldBe "UA"
       ticket.sessionId shouldBe "123"
       ticket.subject shouldBe "Contact form submission"
-      ticket.areaOfTax shouldBe "biztax"
+      ticket.areaOfTax shouldBe "paye"
 
     }
 
+  }
+
+  "Contact Confirmation Page" should {
+    "retrieve the ticket id from the keystore and display it" in new WithContactController {
+
+      import controller._
+      private val keyStoreData: StoredTicket= Map(ticketKey -> "123")
+      when(keyStoreConnector.getEntry[StoredTicket](meq(actionId), meq(source), meq(formId), meq(false))(any(classOf[Manifest[StoredTicket]]), any[HeaderCarrier])).
+        thenReturn(successful(Some(keyStoreData)))
+
+      val result = controller.doThanks(user, FakeRequest())
+
+      val page = Jsoup.parse(contentAsString(result))
+
+      status(result) shouldBe 200
+
+      page.getElementById("ticketId").text() shouldBe "123"
+    }
   }
 
 }
