06da51ee411495078fa6d7da62bd921b3d199c75
Anthony Munene <munene.anthony@gmail.com>
2014-01-17 17:02:48 +0000

    Fixed failing FE tests

--- govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
+++ govuk-tax/modules/common/test/controllers/common/ContactControllerSpec.scala
@@ -97,7 +97,7 @@ class ContactControllerSpec extends BaseSpec with MockitoSugar {
 
       page.getElementsByClass("error-notification").size() shouldBe 3
       page.getElementsByClass("error-notification").get(0).text() shouldBe "Please provide your name"
-      page.getElementsByClass("error-notification").get(1).text() shouldBe "Enter a valid email address."
+      page.getElementsByClass("error-notification").get(1).text() shouldBe "Valid email required"
       page.getElementsByClass("error-notification").get(2).text() shouldBe "Please provide details"
     }
 
