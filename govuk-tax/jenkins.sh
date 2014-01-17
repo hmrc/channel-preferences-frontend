9c8de0a16c698857e3bbede58f4a09485548de84
andygodwin <andy.godwin@digital.cabinet-office.gov.uk>
2014-01-17 11:30:29 +0000

    Changed jenkins build script to support multiple build environments

--- govuk-tax/jenkins.sh
+++ govuk-tax/jenkins.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-if [ $# > 1 ]; then
-        conf_file=$1
+SBT_BOOT_DIR=${HOME}/.sbt/boot/
+SBT_TARGETS="clean test dist publish"
+SBT_STANDARD_OPTS="-Dsbt.log.noformat=true -Xmx1024M -XX:+CMSClassUnloadingEnabled -XX:+UseCompressedOops -XX:MaxPermSize=768m  -Dfile.encoding=UTF8"
+
+if [ -f "/etc/buildrc" ]; then
+  . /etc/buildrc
+else
+  . ./build-env
 fi
 
-. ./build-env
+scriptname=$(basename $0)
+echo "[${scriptname}] Using ivy directory ${HOME}/.ivy2"
+echo "[${scriptname}] java  ${SBT_STANDARD_OPTS} ${SBT_EXTRA_PARAMS} -jar ${SBT_FILE} ${SBT_TARGETS}"
 
-cd govuk-tax
+if [ ! -d "${SBT_BOOT_DIR}" ]; then
+  mkdir -p ${SBT_BOOT_DIR}
+fi
 
-./sbt clean test dist publish
+java    ${SBT_STANDARD_OPTS} ${SBT_EXTRA_PARAMS} \
+        -Dbuild.time="`date`" -Dsbt.boot.directory=${SBT_BOOT_DIR} \
+        -jar ${SBT_FILE} ${SBT_TARGETS}
