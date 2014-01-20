759a8c7c0418521ffeb41b84625cbae7a10be70d
Vaughan Sharman <vaughansharman@hotmail.com>
2014-01-20 12:27:11 +0000

    HMTB-2717 Adding terms and conditions link for sa email sign up journey

---
+++ govuk-tax/modules/common/app/controllers/common/CommonController.scala
@@ -0,0 +1,18 @@
+package controllers.common
+
+import controllers.common.service.Connectors
+import uk.gov.hmrc.common.microservice.auth.AuthConnector
+import controllers.common.actions.Actions
+import uk.gov.hmrc.common.microservice.audit.AuditConnector
+
+
+class CommonController(override val auditConnector: AuditConnector) (implicit override val authConnector: AuthConnector) extends BaseController with Actions with AllRegimeRoots {
+
+  def this() = this()(Connectors.authConnector)
+
+  def termsAndConditions = UnauthorisedAction {
+    implicit request =>
+      Ok(views.html.t_and_c())
+  }
+
+}
