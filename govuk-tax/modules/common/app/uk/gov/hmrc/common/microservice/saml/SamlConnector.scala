e738d2657e009d3f6c9fa127ff80b4231d9d4945
Doug Clinton <doug.clinton@digital.cabinet-office.gov.uk>
2014-01-22 14:18:59 +0000

    [HMTB-2611] The ida token verification call in the saml connector now retrieves the user and password for the API from the Play config and uses them to construct a BasicAuthentication header. If they aren't present in the config then no header will be created.
    The username and password should be AES encrypted with the secret key held as a var in the connector class, and then Base64 encoded.
    
    Change-Id: I8355d7757c93d1c7f4f68fd41f33f98cb686abb1

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/saml/SamlConnector.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/saml/SamlConnector.scala
@@ -7,27 +7,41 @@ import scala.concurrent.Future
 import org.json4s.JObject
 import play.api.Logger
 import scala.util.Try
+import uk.gov.hmrc.crypto.AesCrypto
+import org.apache.commons.codec.binary.Base64
 
 
-class SamlConnector extends Connector {
+class SamlConnector extends Connector with AesCrypto {
+
+  val encryptionKey = "s7fkwn2sc52fggkfslv29g"
 
   override val serviceUrl = MicroServiceConfig.samlServiceUrl
 
-  val idaTokenRequired = MicroServiceConfig.idaTokenRequired
 
   def create(implicit hc: HeaderCarrier) = httpGetF[AuthRequestFormData]("/saml/create")
     .map(_.getOrElse(throw new IllegalStateException("Expected SAML auth response but none returned")))
 
   def validate(authResponse: String)(implicit hc: HeaderCarrier) = {
     httpPostF[AuthResponseValidationResult, Map[String, String]](
-      uri = "/saml/validate",
-      body = Some(Map("samlResponse" -> authResponse))
+    uri = "/saml/validate",
+    body = Some(Map("samlResponse" -> authResponse))
     ).map(_.getOrElse(throw new IllegalStateException("Expected SAML validation response but none returned")))
   }
 
+  val idaTokenRequired = MicroServiceConfig.idaTokenRequired
+  val idaTokenApiUser = MicroServiceConfig.idaTokenApiUser.map(decrypt)
+  val idaTokenApiPass = MicroServiceConfig.idaTokenApiPass.map(decrypt)
+
+  val idaApiBasicAuthHeaders = for {
+    idaUser <- idaTokenApiUser
+    idaPass <- idaTokenApiPass
+  } yield {
+    val userpass = Base64.encodeBase64(s"$idaUser:$idaPass".getBytes)
+    Map("Authorization" -> s"Basic $userpass")
+  }
+
   def validateToken(token: String)(implicit hc: HeaderCarrier): Future[Boolean] = {
-    case class TokenValid(valid: Boolean)
-    httpPostF[JObject, Nothing](s"/ida/tokens/$token/validate", None).map { json =>
+    httpPostF[JObject, Nothing](s"/ida/tokens/$token/validate", None, idaApiBasicAuthHeaders.getOrElse(Map())).map { json =>
       Try {
         json.map { json =>
           implicit val formats = org.json4s.DefaultFormats
