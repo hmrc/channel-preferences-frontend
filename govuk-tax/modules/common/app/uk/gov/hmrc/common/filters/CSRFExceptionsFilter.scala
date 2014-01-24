c316ea20be9c0a3d00383bf8b3cbecbc36105bff
Steve Etherington <s_etherington@hotmail.com>
2014-01-24 10:31:38 +0000

    MR SE [HMTB-2806] view changes for unauthorised problem reporting

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/filters/CSRFExceptionsFilter.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/filters/CSRFExceptionsFilter.scala
@@ -10,7 +10,7 @@ import SessionTimeoutWrapper._
 
 object CSRFExceptionsFilter extends Filter {
 
-  val whitelist = List("/ida/login", "/ssoin", "/contact/problem_reports")
+  val whitelist = List("/ida/login", "/ssoin", "/contact/problem_reports","/contact/problem_reports-unauthenticated")
 
   def apply(f: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] = {
      f(filteredHeaders(rh))
