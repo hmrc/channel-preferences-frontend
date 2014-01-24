58ae715b84b0cc08b51157fe26682a03b783371e
Claudio Scandura <claudio.scandura@digital.cabinet-office.gov.uk>
2014-01-24 16:27:14 +0000

    Trying to fix a test that fails on jenkins only

--- govuk-tax/modules/paye/test/controllers/paye/QuestionnaireAuditorSpec.scala
+++ govuk-tax/modules/paye/test/controllers/paye/QuestionnaireAuditorSpec.scala
@@ -10,6 +10,7 @@ import org.mockito.Matchers
 import scala.concurrent.Future
 import uk.gov.hmrc.common.microservice.audit.AuditEvent
 import play.api.test.FakeRequest
+import controllers.common.actions.HeaderCarrier
 
 
 class QuestionnaireAuditorSpec extends BaseSpec with MockitoSugar {
@@ -20,31 +21,31 @@ class QuestionnaireAuditorSpec extends BaseSpec with MockitoSugar {
 
   "Questionnaire Auditor" should {
 
-    implicit val request = FakeRequest()
-
     //TODO: Figure out why verify is failing on CI and not locally
     "perform the audit and update the keystore if the questionnaire has not been submitted yet" in {
       val mockAuditConnector = mock[AuditConnector]
+      val hc  = HeaderCarrier()
       val mockKeyStoreConnector = mock[KeyStoreConnector]
       val questionnaireAuditor = new QuestionnaireAuditor(mockAuditConnector, mockKeyStoreConnector)
 
       when(mockKeyStoreConnector.getEntry[QuestionnaireData](Matchers.eq("QuestionnaireFormDataSubmission"), Matchers.eq(KeystoreUtils.source), Matchers.eq(transactionId), any())(any(), any())).thenReturn(Future.successful(None))
 
-      questionnaireAuditor.auditOnce(auditEvent, transactionId)
+      questionnaireAuditor.auditOnce(auditEvent, transactionId)(hc)
 
-      verify(mockAuditConnector).audit(Matchers.eq(auditEvent))(any())
+      verify(mockAuditConnector).audit(auditEvent)(hc)
       verify(mockKeyStoreConnector).addKeyStoreEntry[QuestionnaireData](Matchers.eq("QuestionnaireFormDataSubmission"), Matchers.eq(KeystoreUtils.source), Matchers.eq(transactionId), Matchers.eq(QuestionnaireData("HasBeenSubmitted")), Matchers.eq(false))(any(), any())
     }
 
     //TODO: Figure out why verify is failing on CI and not locally
     "not perform the audit when the questionnaire has already been submitted" in {
       val mockAuditConnector = mock[AuditConnector]
+      val hc  = HeaderCarrier()
       val mockKeyStoreConnector = mock[KeyStoreConnector]
       val questionnaireAuditor = new QuestionnaireAuditor(mockAuditConnector, mockKeyStoreConnector)
 
       when(mockKeyStoreConnector.getEntry[QuestionnaireData](Matchers.eq("QuestionnaireFormDataSubmission"), Matchers.eq(KeystoreUtils.source), Matchers.eq(transactionId), any())(any(), any())).thenReturn(Future.successful(Some(QuestionnaireData("populated!"))))
 
-      questionnaireAuditor.auditOnce(auditEvent, transactionId)
+      questionnaireAuditor.auditOnce(auditEvent, transactionId)(hc)
 
       verifyZeroInteractions(mockAuditConnector)
       verify(mockKeyStoreConnector, never()).addKeyStoreEntry[QuestionnaireData](any(), any(), any(), any(), any())(any(), any())
