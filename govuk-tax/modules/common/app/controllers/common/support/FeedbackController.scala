93ab4d6921131520fbb58b44883267b7f2350ec5
Anthony Munene <munene.anthony@gmail.com>
2014-01-27 12:20:37 +0000

    HMTB-2540 text copy amend

--- govuk-tax/modules/common/app/controllers/common/support/FeedbackController.scala
+++ govuk-tax/modules/common/app/controllers/common/support/FeedbackController.scala
@@ -109,5 +109,5 @@ object FeedbackForm {
 
 object FeedbackFormConfig {
   val validExperiences = (5 to 1 by -1) map (_.toString)
-  val feedbackRatings = validExperiences zip Seq("Very good", "Good", "Unsure", "Bad", "Very bad")
+  val feedbackRatings = validExperiences zip Seq("Very good", "Good", "Neutral", "Bad", "Very bad")
 }
