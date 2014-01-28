250b9c43cf5b0d27bb8cfc709e57200ff8f8c2e7
tomvanneerijnen <tom.vanneerijnen@digital.cabinet-office.gov.uk>
2014-01-28 10:42:45 +0000

    Update start-docker.sh
    bumped govuk-tax version number to match current version

--- govuk-tax/start-docker.sh
+++ govuk-tax/start-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-exec /src/govuk-tax-0.0.1-SNAPSHOT/bin/govuk-tax \
+exec /src/govuk-tax-1.0.1-SNAPSHOT/bin/govuk-tax \
   -Dhttp.port=8080 \
   -Dapplication.log=INFO \
   -Dlogger.resource=/govuk-tax-logger.xml \
