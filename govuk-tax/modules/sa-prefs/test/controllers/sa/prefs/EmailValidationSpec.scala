57f0a6fe78b4ec825f37e2035971d0aa7784a315
Anthony Munene <munene.anthony@gmail.com>
2014-01-31 15:30:01 +0000

    HMTB-2885 Verification expired copy text change

--- govuk-tax/modules/sa-prefs/test/controllers/sa/prefs/EmailValidationSpec.scala
+++ govuk-tax/modules/sa-prefs/test/controllers/sa/prefs/EmailValidationSpec.scala
@@ -62,7 +62,7 @@ class EmailValidationSpec extends WordSpec with ShouldMatchers with MockitoSugar
       val html = contentAsString(response)
       html shouldNot include("portalHomeLink/home")
       val page = Jsoup.parse(html)
-      page.getElementsByTag("h2").first.text shouldBe "Looks like we have a problem..."
+      page.getElementsByTag("h2").first.text shouldBe "This verification link has expired"
       page.getElementsByTag("p").text should include("out of date")
       verify(controller.preferencesMicroService).updateEmailValidationStatusUnsecured(meq(token))
     }
