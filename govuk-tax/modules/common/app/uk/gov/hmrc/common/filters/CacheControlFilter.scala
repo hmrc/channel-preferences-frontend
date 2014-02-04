decb0a5bf6001418ce7df2fc63f73a694f374203
Howy Perrin <howard.perrin@digital.cabinet-office.gov.uk>
2014-02-04 11:24:48 +0000

    HMTB-2899 Move cacheable content type list into config

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/filters/CacheControlFilter.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/filters/CacheControlFilter.scala
@@ -4,9 +4,13 @@ import play.api.mvc.{SimpleResult, RequestHeader, Filter}
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.mvc.Http.{Status, HeaderNames}
+import play.api.{Play, Logger}
+import scala.collection.JavaConverters._
 
-case class CacheControlFilter(cachableContentTypes: String*) extends Filter {
-  def apply(next: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] = {
+abstract class CacheControlFilter extends Filter {
+  val cachableContentTypes: Seq[String]
+
+  final def apply(next: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] = {
     next(rh).map(r =>
       (r.header.status, r.header.headers.get(HeaderNames.CONTENT_TYPE)) match {
         case (Status.NOT_MODIFIED, _) => r
@@ -14,5 +18,17 @@ case class CacheControlFilter(cachableContentTypes: String*) extends Filter {
         case _ => r.withHeaders(HeaderNames.CACHE_CONTROL -> "no-cache,no-store,max-age=0")
       }
     )
+
+  }
+}
+object CacheControlFilter {
+  def fromConfig(configKey: String) = {
+    new CacheControlFilter {
+      override lazy val cachableContentTypes = {
+        val c = Play.current.configuration.getStringList(configKey).toList.map(_.asScala).flatten
+        Logger.info(s"Will allow caching of content types matching: ${c.mkString(", ")}")
+        c
+      }
+    }
   }
 }
