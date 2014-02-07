9bcdd35c0e8725b4167242582c8e21b89102d68d
Howy Perrin <howard.perrin@digital.cabinet-office.gov.uk>
2014-02-07 09:36:21 +0000

    Work in progress

--- govuk-tax/modules/common/app/views/helpers/Link.scala
+++ govuk-tax/modules/common/app/views/helpers/Link.scala
@@ -5,7 +5,7 @@ import play.api.i18n.Messages
 
 
 object PortalLink {
-  def apply(url: String) = Link(url, None, sso = true)
+  def apply(url: String) = Link(url, None, sso = HasSso)
 }
 
 object InternalLink {
@@ -13,30 +13,50 @@ object InternalLink {
 }
 
 object ExternalLink {
-  def apply(url: String) = Link(url, None, newWindow = true)
+  def apply(url: String) = Link(url, None, target = NewWindow)
+}
+
+trait Target {
+  protected val targetName: String
+  val toAttr = Link.attr("target", targetName)
+}
+case object SameWindow extends Target {
+  override val targetName = "_self"
+}
+case object NewWindow extends Target {
+  override val targetName = "_blank"
+}
+
+trait PossibleSso {
+  protected val value: String
+  val toAttr = Link.attr("data-sso", value)
+}
+case object NoSso extends PossibleSso {
+  override val value = "false"
+}
+case object HasSso extends PossibleSso {
+  override val value = "true"
 }
 
 case class Link(url: String,
                 value: Option[String],
                 id: Option[String] = None,
-                newWindow: Boolean = false,
-                sso: Boolean = false,
+                target: Target = SameWindow,
+                sso: PossibleSso = NoSso,
                 cssClasses: Option[String] = None) {
 
   import Link._
 
   private def hrefAttr = attr("href", url)
   private def idAttr = id.map(attr("id", _)).getOrElse("")
-  private def targetAttr = if (newWindow) attr("target", "_blank") else attr("target", "_self")
-  private def ssoAttr = attr("data-sso", String.valueOf(sso))
   private def text = value.map(v => escape(Messages(v))).getOrElse("")
   private def cssAttr = cssClasses.map(attr("class", _)).getOrElse("")
 
-  def toHtml = Html(s"<a$idAttr$hrefAttr$targetAttr$ssoAttr$cssAttr>$text</a>")
+  def toHtml = Html(s"<a$idAttr$hrefAttr${target.toAttr}${sso.toAttr}$cssAttr>$text</a>")
 }
 
 object Link {
   private def escape(str: String) = HtmlFormat.escape(str).toString()
 
-  private def attr(name: String, value: String) = s""" $name="${escape(value)}""""
+  def attr(name: String, value: String) = s""" $name="${escape(value)}""""
 }
