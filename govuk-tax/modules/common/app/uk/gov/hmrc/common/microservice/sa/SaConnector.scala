4bb63995cf8e53ec32aa0bdd163926b049705a2e
Tim Goodchild <tgoodchild@equalexperts.com>
2014-02-25 14:41:39 +0000

    HMTB-3028 [TG & MR] remove dead code for updateMainAddress

--- govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/sa/SaConnector.scala
+++ govuk-tax/modules/common/app/uk/gov/hmrc/common/microservice/sa/SaConnector.scala
@@ -1,9 +1,7 @@
 package uk.gov.hmrc.common.microservice.sa
 
-import uk.gov.hmrc.common.microservice.{Connector, MicroServiceConfig, MicroServiceException}
-import play.api.libs.json.Json
-import controllers.common.domain.Transform._
-import uk.gov.hmrc.common.microservice.sa.domain.write.{TransactionId, SaAddressForUpdate}
+import uk.gov.hmrc.common.microservice.{Connector, MicroServiceConfig}
+import uk.gov.hmrc.common.microservice.sa.domain.write.SaAddressForUpdate
 import uk.gov.hmrc.common.microservice.sa.domain.{SaAccountSummary, SaPerson, SaJsonRoot}
 import scala.concurrent._
 import controllers.common.actions.HeaderCarrier
@@ -17,14 +15,4 @@ class SaConnector extends Connector {
   def person(uri: String)(implicit hc: HeaderCarrier): Future[Option[SaPerson]] = httpGetF[SaPerson](uri)
 
   def accountSummary(uri: String)(implicit headerCarrier: HeaderCarrier): Future[Option[SaAccountSummary]] = httpGetF[SaAccountSummary](uri)
-
-  def updateMainAddress(uri: String, mainAddress: SaAddressForUpdate)(implicit hc: HeaderCarrier): Future[Either[String, TransactionId]] = {
-    httpPost(uri, Json.parse(toRequestBody(mainAddress))) { response =>
-      response.status match {
-        case 202 => Right(extractJSONResponse[TransactionId](response))
-        case 409 => Left("A previous details change is already being processed, this will take up to 48 hours to complete.")
-        case _ => throw new MicroServiceException("Error updating main address", response)
-      }
-    }
-  }
 }
