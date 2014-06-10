7caa9cd86cd6114ad110651a9d6c948bacfa332c
Eric Vlaanderen <eric.vlaanderen@digital.cabinet-office.gov.uk>
2014-06-10 17:25:43 +0100

    Fix up logout url for sa-prefs

--- app/controllers/sa/prefs/ExternalUrls.scala
+++ app/controllers/sa/prefs/ExternalUrls.scala
@@ -5,7 +5,8 @@ import play.api.Play
 
 object ExternalUrls extends RunMode {
   import play.api.Play.current
-  val ytaHost        = s"${Play.configuration.getString(s"govuk-tax.$env.yta.host").getOrElse("")}"
-  val accountDetails      = s"$ytaHost/account/account-details"
-  val businessTaxHome   = s"$ytaHost/account"
+  val ytaHost          = s"${Play.configuration.getString(s"govuk-tax.$env.yta.host").getOrElse("")}"
+  val accountDetails    = s"$ytaHost/account/account-details"
+  val businessTaxHome = s"$ytaHost/account"
+  val signOut          = s"$ytaHost/sign-out"
 }
