ca959f0fcd49d4feaea9f3c7a875d1e3778d9a77
Eric Vlaanderen <eric.vlaanderen@digital.cabinet-office.gov.uk>
2014-06-13 12:01:54 +0100

    Rename to preferences-frontend

--- jenkins-release.sh
+++ jenkins-release.sh
@@ -9,6 +9,6 @@ fi
 scriptname=$(basename $0)
 echo "[${scriptname}] Using ivy directory ${HOME}/.ivy2"
 
-export SA_PREFS_VERSION=`git describe --abbrev=0 --match release/* | sed s/release\\\///`
+export PREFERENCES_FRONTEND_VERSION=`git describe --abbrev=0 --match release/* | sed s/release\\\///`
 
 ./sbt -Dsbt.log.noformat=true clean dist publish
