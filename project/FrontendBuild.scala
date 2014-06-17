70f5ce58eda2092270fb8ab1e96f2dbb97b0cad2
Eric Vlaanderen <eric.vlaanderen@digital.cabinet-office.gov.uk>
2014-06-17 12:43:24 +0100

    remove uneeded dependencies

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -10,9 +10,7 @@ object FrontendBuild extends Build {
   val appName = "preferences-frontend"
   val appVersion = envOrElse("PREFERENCES_FRONTEND_VERSION", "999-SNAPSHOT")
 
-  val appSpecificDependencies = Seq(
-    "com.netaporter" %% "scala-uri" % "0.4.0" exclude("com.typesafe.sbt", "sbt-pgp") exclude("com.github.scct", "scct_2.10"),
-    "com.github.scct" %% "scct" % "0.2.1")
+  val appSpecificDependencies = Seq("com.netaporter" %% "scala-uri" % "0.4.0" exclude("com.typesafe.sbt", "sbt-pgp") exclude("com.github.scct", "scct_2.10"))
   val dependencies = requiredDependencies ++ appSpecificDependencies
   lazy val microservice = PlayMicroServiceBuild(appName,
     appVersion,
@@ -28,26 +26,22 @@ private object Dependencies {
   private val govukTemplateVersion = "1.5.0"
   private val playFrontendVersion = "3.5.1"
   private val playMicroServiceVersion = "1.14.0"
-  private val taxCoreVersion = "3.6.1"
 
   private val metricsGraphiteVersion = "3.0.1"
   private val pegdownVersion = "1.4.2"
   private val playMetricsVersion = "0.1.3"
   private val scalaTestVersion = "2.1.7"
-  private val playTestVersion = "2.2.3"
   private val jsoupVersion = "1.7.2"
   private val mockitoVersion = "1.9.5"
 
   val govukTemplate = "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion
   val playFrontend = "uk.gov.hmrc" %% "play-frontend" % playFrontendVersion
   val playMicroservice = "uk.gov.hmrc" %% "play-microservice" % playMicroServiceVersion
-  val taxCore = "uk.gov.hmrc" %% "tax-core" % taxCoreVersion
 
   val metricsGraphite = "com.codahale.metrics" % "metrics-graphite" % metricsGraphiteVersion
   val pegdown = "org.pegdown" % "pegdown" % pegdownVersion
   val playMetrics = "com.kenshoo" %% "metrics-play" % playMetricsVersion
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
-  val playTest = "com.typesafe.play" %% "play-test" % playTestVersion
   val jsoup = "org.jsoup" % "jsoup" % jsoupVersion
   val mockito = "org.mockito" % "mockito-all" % mockitoVersion
 
@@ -58,18 +52,15 @@ private object Dependencies {
     compile(playMicroservice),
     compile(govukTemplate),
     compile(playFrontend),
-    compile(taxCore),
 
     test(jsoup),
     test(scalaTest),
     test(pegdown),
     test(playMicroservice, Seq("tests")),
-    test(playFrontend),
     test(mockito),
 
     integrationTest(scalaTest),
     integrationTest(pegdown),
-    integrationTest(playTest),
     integrationTest(playMicroservice, Seq("tests", "tests-sources", "tests-javadoc"))
   )
 }
