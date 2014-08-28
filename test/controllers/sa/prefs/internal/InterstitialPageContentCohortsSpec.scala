e05339adc9d982995d438e6b69ecbd12b30260f5
tejaswitatakawale <tejaswita.takawale@digital.cabinet-office.gov.uk>
2014-08-28 10:05:25 +0100

    SA-108: adding test for interstitialpageconnectorcohorts
    Yet to implement them

---
+++ test/controllers/sa/prefs/internal/InterstitialPageContentCohortsSpec.scala
@@ -0,0 +1,14 @@
+package controllers.sa.prefs.internal
+
+import uk.gov.hmrc.test.UnitSpec
+
+class InterstitialPageContentCohortsSpec extends UnitSpec {
+
+   "cohort value" should {
+
+    "always be the same for a given user" in pending
+    "be evenly spread for given set of users" in pending
+
+  }
+
+}
