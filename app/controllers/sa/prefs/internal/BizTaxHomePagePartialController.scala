7877fa6db4599ed6722567e3c51f9f7aaaa2d5bc
tejaswitatakawale <tejaswita.takawale@digital.cabinet-office.gov.uk>
2014-12-02 12:15:35 +0000

    SA-341 Add partial for pending verification email warning
    Initial commit, add constructor

--- app/controllers/sa/prefs/internal/BizTaxHomePagePartialController.scala
+++ app/controllers/sa/prefs/internal/BizTaxHomePagePartialController.scala
@@ -1,15 +1,19 @@
 package controllers.sa.prefs.internal
 
+import connectors.PreferencesConnector
 import controllers.common.BaseController
 import controllers.common.actions.Actions
+import controllers.common.service.Connectors
 import play.api.mvc.Action
 import uk.gov.hmrc.common.microservice.auth.AuthConnector
 
 import scala.concurrent.Future
 
-class BizTaxHomePagePartialController(implicit override val authConnector: AuthConnector)
+class BizTaxHomePagePartialController(val preferenceConnector: PreferencesConnector)(implicit override val authConnector: AuthConnector)
   extends BaseController
   with Actions {
+
+  def this() = this(PreferencesConnector)(Connectors.authConnector)
 
   def preferencesWarning() = Action.async { implicit request =>
     Future.successful(Ok)
