00ff8f0ea4cace697635b19fc23ed6457bddee1c
Nuno Marques <nmarques@equalexperts.com>
2015-03-24 18:18:40 +0000

    SA-673 updated url-builder to 0.7.0 to allow voice over to read links that open in another tab/window

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -33,7 +33,7 @@ private object Dependencies {
     "uk.gov.hmrc"    %% "play-authorisation"  % "0.8.0",
     "uk.gov.hmrc"    %% "play-health"    % "0.7.0",
     "uk.gov.hmrc"    %% "emailaddress"   % "0.2.0",
-    "uk.gov.hmrc"    %% "url-builder"    % "0.6.0",
+    "uk.gov.hmrc"    %% "url-builder"    % "0.7.0",
     "uk.gov.hmrc"    %% "crypto"         % "1.6.0",
     "uk.gov.hmrc"    %% "play-ui"        % "1.8.0",
     "com.netaporter" %% "scala-uri"      % "0.4.2",
