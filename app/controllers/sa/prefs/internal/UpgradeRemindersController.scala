f588a360192f9b54cca931e30ee7ea99d650e493
rich <rjohnston@equalexperts.com>
2015-06-09 16:05:57 +0100

    SA-803 Honour returnUrl on the upgrade url

--- app/controllers/sa/prefs/internal/UpgradeRemindersController.scala
+++ app/controllers/sa/prefs/internal/UpgradeRemindersController.scala
@@ -4,7 +4,7 @@ import connectors.PreferencesConnector
 import controllers.sa.prefs.ExternalUrls.yourIncomeTax
 import controllers.sa.prefs.SaRegimeWithoutRedirection
 import controllers.sa.prefs.config.Global
-import play.api.mvc.{Action, AnyContent, Request}
+import play.api.mvc.{Result, Action, AnyContent, Request}
 import play.twirl.api.Html
 import uk.gov.hmrc.domain.{Nino, SaUtr}
 import uk.gov.hmrc.emailaddress.ObfuscatedEmailAddress
@@ -31,11 +31,16 @@ trait UpgradeRemindersController extends FrontendController with Actions {
       renderUpgradePageIfPreferencesAvailable(authContext.principal.accounts.sa.get.utr, authContext.principal.accounts.paye.map(_.nino))
   }
 
-  private[controllers] def renderUpgradePageIfPreferencesAvailable(utr: SaUtr, maybeNino: Option[Nino])(implicit request: Request[AnyContent]) = {
-    preferencesConnector.getPreferences(utr, maybeNino).map {
-      case Some(prefs) => Ok(upgrade_printing_preferences(utr, maybeNino, prefs.email.map(e => ObfuscatedEmailAddress(e.email)), yourIncomeTax))
-      case _ => NotFound
+  private[controllers] def renderUpgradePageIfPreferencesAvailable(utr: SaUtr, maybeNino: Option[Nino])(implicit request: Request[AnyContent]): Future[Result] = {
+    request.getQueryString("returnUrl") match {
+      case Some(returnUrl) =>
+        preferencesConnector.getPreferences(utr, maybeNino).map {
+          case Some(prefs) => Ok(upgrade_printing_preferences(utr, maybeNino, prefs.email.map(e => ObfuscatedEmailAddress(e.email)), returnUrl))
+          case _ => NotFound
+        }
+      case _ => Future.successful(BadRequest("returnUrl parameter missing"))
     }
+
   }
 
   def upgrade(returnUrl: String) = AuthorisedFor(SaRegimeWithoutRedirection).async {
