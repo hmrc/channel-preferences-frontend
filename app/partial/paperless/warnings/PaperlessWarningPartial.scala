9ffdf883bf630f5d7f6f87c49d80468536e81415
Mohammed Abdulrazeg <github@mzawi.me>
2015-11-02 10:57:47 +0000

    DC-38 Updated routes and method signature to match the refactoring of HostContext

--- app/partial/paperless/warnings/PaperlessWarningPartial.scala
+++ app/partial/paperless/warnings/PaperlessWarningPartial.scala
@@ -5,9 +5,9 @@ import connectors.{SaEmailPreference, SaPreference}
 import play.twirl.api.HtmlFormat
 
 object PaperlessWarningPartial {
-  def apply(prefs: SaPreference) = prefs match {
-    case SaPreference(_, Some(email@SaEmailPreference(_, Status.Pending, _, _, _)))     => html.pending_email_verification(email)
-    case SaPreference(_, Some(SaEmailPreference(_, Status.Bounced, mailBoxFull, _, _))) => html.bounced_email(mailBoxFull)
+  def apply(prefs: SaPreference, returnUrl: String, returnLinkText: String) = prefs match {
+    case SaPreference(_, Some(email@SaEmailPreference(_, Status.Pending, _, _, _)))     => html.pending_email_verification(email, returnUrl, returnLinkText)
+    case SaPreference(_, Some(SaEmailPreference(_, Status.Bounced, mailBoxFull, _, _))) => html.bounced_email(mailBoxFull, returnUrl, returnLinkText)
     case _                                                                              => HtmlFormat.empty
   }
 }
