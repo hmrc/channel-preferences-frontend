9b583740a168fdbc7b68416b289c8d33c365fd3f
Jianfeng Tian <jianfeng.tian@digital.hmrc.gov.uk>
2016-02-26 12:03:24 +0000

    DC-193 use the full url instead of the relative one

--- app/controllers/internal/ActivationController.scala
+++ app/controllers/internal/ActivationController.scala
@@ -2,8 +2,9 @@ package controllers.internal
 
 import config.Global
 import connectors.EntityResolverConnector
+import controllers.ExternalUrlPrefixes
 import model.{Encrypted, FormType, HostContext}
-import play.api.libs.json.{JsString, Json}
+import play.api.libs.json.Json
 import play.api.mvc.Action
 import uk.gov.hmrc.play.config.AppName
 import uk.gov.hmrc.play.frontend.auth.Actions
@@ -15,11 +16,15 @@ object ActivationController extends ActivationController {
   override val entityResolverConnector: EntityResolverConnector = EntityResolverConnector
 
   override protected implicit val authConnector: AuthConnector = Global.authConnector
+
+  val hostUrl = ExternalUrlPrefixes.pfUrlPrefix
 }
 
 trait ActivationController extends FrontendController with Actions with AppName {
 
   def entityResolverConnector: EntityResolverConnector
+
+  val hostUrl: String
 
   def activate(formType: FormType, taxIdentifier: String, hostContext: HostContext) = Action.async(parse.json) { implicit request =>
     entityResolverConnector.activate(formType, taxIdentifier, hostContext, request.body) map { result =>
@@ -27,8 +32,8 @@ trait ActivationController extends FrontendController with Actions with AppName 
       result.status match {
         case 412 => {
           val redirectUrl: String = result.body match {
-            case "OptInRequired" => controllers.internal.routes.ChoosePaperlessController.redirectToDisplayFormWithCohort(None, hostContext).url
-            case "UpgradeRequired" => controllers.internal.routes.UpgradeRemindersController.displayUpgradeForm(Encrypted(hostContext.returnUrl)).url
+            case "OptInRequired" => hostUrl + controllers.internal.routes.ChoosePaperlessController.redirectToDisplayFormWithCohort(None, hostContext).url
+            case "UpgradeRequired" => hostUrl + controllers.internal.routes.UpgradeRemindersController.displayUpgradeForm(Encrypted(hostContext.returnUrl)).url
           }
           Status(result.status)(Json.obj("redirectUserTo" -> redirectUrl))
         }
--- it/ActivateISpec.scala
+++ it/ActivateISpec.scala
@@ -1,24 +1,22 @@
-import org.scalatest.mock.MockitoSugar
 import play.api.test.Helpers._
+import uk.gov.hmrc.domain.SaUtr
 
-class ActivateISpec extends PreferencesFrontEndServer with EmailSupport with MockitoSugar {
+class ActivateISpec extends PreferencesFrontEndServer {
 
   "activate" should {
     "return PRECONDITION_FAILED with redirectUserTo link if activating sa-all for a new user" in new TestCaseWithFrontEndAuthentication {
-      val saUtr = Generate.utr
 
-      val response = `/paperless/activate/:form-type/:tax-identifier`("sa-all", saUtr)().put().futureValue
+      val response = `/paperless/activate/:form-type/:tax-identifier`("sa-all", SaUtr(utr))().put().futureValue
       response.status should be (PRECONDITION_FAILED)
 
-      (response.json \ "redirectUserTo").as[String] should be (s"/paperless/choose?returnUrl=$encryptedReturnUrl&returnLinkText=$encryptedReturnText")
+      (response.json \ "redirectUserTo").as[String] should be (s"http://localhost:9024/paperless/choose?returnUrl=$encryptedReturnUrl&returnLinkText=$encryptedReturnText")
     }
 
     "return PRECONDITION_FAILED with redirectUserTo link if activating notice-of-coding for a new user with given utr and nino" in new TestCaseWithFrontEndAuthentication {
       val nino = Generate.nino
-      val saUtr = Generate.utr
-      val response = `/paperless/activate/:form-type/:tax-identifier`("notice-of-coding", nino)(saUtr).put().futureValue
+      val response = `/paperless/activate/:form-type/:tax-identifier`("notice-of-coding", nino)(SaUtr(utr)).put().futureValue
       response.status should be (PRECONDITION_FAILED)
-      (response.json \ "redirectUserTo").as[String] should be (s"/paperless/choose?returnUrl=$encryptedReturnUrl&returnLinkText=$encryptedReturnText")
+      (response.json \ "redirectUserTo").as[String] should be (s"http://localhost:9024/paperless/choose?returnUrl=$encryptedReturnUrl&returnLinkText=$encryptedReturnText")
     }
   }
 }
