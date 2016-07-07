25fe46f7ffb2396d2710aaed3d5356e008f5585a
rich <rjohnston@equalexperts.com>
2016-07-07 13:33:27 +0100

    DC-493 rename badly named tests

--- it/ActivateISpec.scala
+++ it/ActivateISpec.scala
@@ -4,7 +4,7 @@ import uk.gov.hmrc.domain.{Vrn, CtUtr, SaUtr}
 class ActivateISpec extends PreferencesFrontEndServer {
 
   "activate" should {
-    "return PRECONDITION_FAILED with redirectUserTo link if activating sa-all for a new user with utr only" in new TestCaseWithFrontEndAuthentication {
+    "return PRECONDITION_FAILED with redirectUserTo link if activating for a new user with utr only" in new TestCaseWithFrontEndAuthentication {
 
       val response = `/paperless/activate`(SaUtr(utr))().put().futureValue
       response.status should be (PRECONDITION_FAILED)
@@ -12,13 +12,13 @@ class ActivateISpec extends PreferencesFrontEndServer {
       (response.json \ "redirectUserTo").as[String] should be (s"http://localhost:9024/paperless/choose?returnUrl=$encryptedReturnUrl&returnLinkText=$encryptedReturnText")
     }
 
-    "return PRECONDITION_FAILED with redirectUserTo link if activating notice-of-coding for a new user with given utr and nino" in new TestCaseWithFrontEndAuthentication {
+    "return PRECONDITION_FAILED with redirectUserTo link if activating for a new user with given utr and nino" in new TestCaseWithFrontEndAuthentication {
       val response = `/paperless/activate`(nino)(SaUtr(utr)).put().futureValue
       response.status should be (PRECONDITION_FAILED)
       (response.json \ "redirectUserTo").as[String] should be (s"http://localhost:9024/paperless/choose?returnUrl=$encryptedReturnUrl&returnLinkText=$encryptedReturnText")
     }
 
-    "return PRECONDITION_FAILED with redirectUserTo link if activating for a user with no nino or utr" in new TestCaseWithFrontEndAuthentication {
+    "return UNAUTHORIZED if activating for a user with no nino or utr" in new TestCaseWithFrontEndAuthentication {
       val response = `/paperless/activate`(CtUtr("TEST_USER"))(Vrn("ANOTHER_TEST")).put().futureValue
       response.status should be (UNAUTHORIZED)
     }
