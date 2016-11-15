54cbde412a88e9ee8cc735fe25174bf0ecccf042
Enrique Faci <enrique.faci@gmail.com>
2016-11-15 15:56:01 +0000

    DC-679: fix static public assets

--- app/controllers/internal/PlatformMonitoringController.scala
+++ app/controllers/internal/PlatformMonitoringController.scala
@@ -1,21 +1,11 @@
 package controllers.internal
 
-import java.io.File
-import javax.inject.Inject
-
-import play.api.Environment
-import play.api.mvc.{Action, Result}
+import controllers.Assets
 import uk.gov.hmrc.play.config.AppName
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 
-import scala.concurrent.Future
+// DC-679: Moving monitoring to new controller because we require to disable auditing.
+class PlatformHealthCheckController extends FrontendController with AppName {
 
-// DC-679: Moving monitoring to new controller because controller.Assets.at() does not load resources from /public folder anymore.
-class PlatformHealthCheckController  @Inject()(env: Environment) extends FrontendController with AppName {
-
-    def getAsset(fileName: String) = Action.async { implicit request =>
-      Future.successful(
-        Option(env.classLoader.getResource(s"public/$fileName")).fold[Result](NotFound)(f => Ok.sendFile(new File(f.toURI)))
-      )
-    }
- }
+    def getAsset(fileName: String) = Assets.at(path="/public", file = fileName)
+}
