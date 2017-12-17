f3711c056fc021fda26781c86aac426846439a36
Jim Collins <jim.collins@digital.hmrc.gov.uk>
2017-12-17 14:20:31 +0000

    DC-1246 Use POST endpoint for validating an email

--- test/connectors/EmailConnectorSpec.scala
+++ test/connectors/EmailConnectorSpec.scala
@@ -4,13 +4,13 @@ import helpers.ConfigHelper
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.OneAppPerSuite
 import play.api.Application
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, Writes}
 import uk.gov.hmrc.play.audit.http.HttpAuditing
 import uk.gov.hmrc.play.config.ServicesConfig
 import uk.gov.hmrc.play.test._
 
 import scala.concurrent.Future
-import uk.gov.hmrc.http.{ HeaderCarrier, HttpResponse }
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.http.hooks.HttpHook
 
 class EmailConnectorSpec extends UnitSpec with ScalaFutures with OneAppPerSuite {
@@ -39,15 +39,19 @@ class EmailConnectorSpec extends UnitSpec with ScalaFutures with OneAppPerSuite 
       val connector = new EmailConnector with HttpAuditing with ServicesConfig {
         protected def serviceUrl = "http://email.service:80"
 
-        def doGet(url: String)(implicit hc: HeaderCarrier) = {
-          Future.successful(responseFromEmailService)
-        }
-
         val hooks: Seq[HttpHook] = Seq(AuditingHook)
 
         def auditConnector = ???
 
         override def configuration = ???
+
+        override def doPost[A](url: String, body: A, headers: Seq[(String, String)])(implicit wts: Writes[A], hc: HeaderCarrier): Future[HttpResponse]= Future.successful(responseFromEmailService)
+
+        override def doPostString(url: String, body: String, headers: Seq[(String, String)])(implicit hc: HeaderCarrier) = ???
+
+        override def doEmptyPost[A](url: String)(implicit hc: HeaderCarrier) = ???
+
+        override def doFormPost(url: String, body: Map[String, Seq[String]])(implicit hc: HeaderCarrier) = ???
       }
 
       val exampleEmailAddress = "bob@somewhere.com"
@@ -58,15 +62,19 @@ class EmailConnectorSpec extends UnitSpec with ScalaFutures with OneAppPerSuite 
       override val connector = new EmailConnector with HttpAuditing with ServicesConfig {
         protected def serviceUrl = "http://email.service:80"
 
-        def doGet(url: String)(implicit hc: HeaderCarrier) = {
-          Future.failed(new Exception("Service down"))
-        }
-
         val hooks: Seq[HttpHook] = Seq(AuditingHook)
 
         def auditConnector = ???
 
         override def configuration = ???
+
+        override def doPost[A](url: String, body: A, headers: Seq[(String, String)])(implicit wts: Writes[A], hc: HeaderCarrier): Future[HttpResponse] = Future.failed(new Exception("Service down"))
+
+        override def doPostString(url: String, body: String, headers: Seq[(String, String)])(implicit hc: HeaderCarrier) = ???
+
+        override def doEmptyPost[A](url: String)(implicit hc: HeaderCarrier) = ???
+
+        override def doFormPost(url: String, body: Map[String, Seq[String]])(implicit hc: HeaderCarrier) = ???
       }
 
     }
