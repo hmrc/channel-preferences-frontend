6aa206a0f3a01f3623cfc8e7e528ab1d2a9c743e
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-06-10 16:14:36 +0100

    [DC-1950] Remove unused functional tests with deprecated auth api

--- functional/pages/InternalPagesSetup.scala
+++
@@ -1,8 +0,0 @@
-package pages
-
-import uk.gov.hmrc.endtoend.sa.{RelativeUrl, Page, AbsoluteUrl}
-
-object InternalPagesSetup {
-  trait InternalPage extends Page with RelativeUrl
-  implicit def internalPageUrls[T <: InternalPage] = AbsoluteUrl.fromRelativeUrl[T](host = "localhost", port = 9000)
-}
