d6166bf382b229fff5a0936f0d70cf30e752f711
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2021-01-08 10:17:32 +0000

    DC-3062 update play-frontend-hmrc version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.2.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.34.0-play-27",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.35.0-play-27",
     "uk.gov.hmrc" %% "play-frontend-govuk"        % "0.56.0-play-27"
   )
 
