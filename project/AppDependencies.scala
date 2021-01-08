f269345a2d35e760441be939c12bb8870639ea5a
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2021-01-08 14:16:07 +0000

    DC-3062 - swagger build failure fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.2.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.35.0-play-27",
-    "uk.gov.hmrc" %% "play-frontend-govuk"        % "0.56.0-play-27"
+    "uk.gov.hmrc" %% "play-frontend-govuk"        % "0.57.0-play-27"
   )
 
   val test = Seq(
