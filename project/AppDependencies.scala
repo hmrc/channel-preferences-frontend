4dadd2bd5c9cd70cc85185fe82ecd0eddf4d8d73
Valy Dia <2129101+valydia@users.noreply.github.com>
2021-06-14 17:14:35 +0100

    DC-3489: Update sbt resolvers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -27,8 +27,8 @@ object AppDependencies {
     "uk.gov.hmrc"   %% "play-frontend-govuk"        % "0.60.0-play-27",
     "uk.gov.hmrc"   %% "play-language"              % "4.10.0-play-27",
     "com.iterable"  %% "swagger-play"               % "2.0.1",
-    "com.iheart"    %% "play-swagger"               % "0.10.2",
-    "org.typelevel" %% "cats-core"                  % "2.3.1"
+    "com.iheart"    %% "play-swagger"               % "0.10.5",
+    "org.typelevel" %% "cats-core"                  % "2.6.1"
   )
 
   val test = Seq(
@@ -38,10 +38,10 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"              % current % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "4.0.3" % "test, it",
     "org.jsoup"              % "jsoup"                   % "1.13.1" % Test,
-    "org.mockito"            % "mockito-core"            % "3.7.7",
+    "org.mockito"            % "mockito-core"            % "3.11.1",
     "com.vladsch.flexmark"   % "flexmark-all"            % "0.36.8" % "test, it",
     "org.pegdown"            % "pegdown"                 % "1.6.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8"           % "2.27.2" % "test,it"
+    "com.github.tomakehurst" % "wiremock-jre8"           % "2.28.1" % "test,it"
   )
 
   val dependencyOverrides = Seq(
